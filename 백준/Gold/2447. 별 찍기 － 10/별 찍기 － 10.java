4ace1ff54d34d41f7321454f1a77393fd3a373ac
zhu <71738723+some1mine@users.noreply.github.com>
2022-12-02 00:49:06 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 404 ms, Memory: 16140 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -8,13 +8,13 @@ public class Main {
         int n = Integer.parseInt(bufferedReader.readLine());
 
         for(int i = 0 ; i < n ; i += 1) {
-           for(int j = 0 ; j < n ; j += 1) solve(i, j); // 줄 단위로 계산
+           for(int j = 0 ; j < n ; j += 1) printLine(i, j); // 줄 단위로 계산
            bufferedWriter.write("\n"); // 한 줄씩 출력
         }
         bufferedWriter.flush();
         bufferedWriter.close();
     }
-    public static void solve(int x, int y) throws IOException {
+    public static void printLine(int x, int y) throws IOException {
         while(true) {
             if(x == 0) break;
             if(x % 3 == 1 && y % 3 == 1) {
