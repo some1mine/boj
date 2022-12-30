2357c1bfc64fccc7e80d2154df0a9e1ba2af4d2c
zhu <71738723+some1mine@users.noreply.github.com>
2022-12-30 22:44:30 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] common) {
         int answer = 0;
-        if(isEmdck(common[0], common[1], common[2])) {
+        if(check(common[0], common[1], common[2])) {
             answer = common[common.length-1] + findRhdck(common[0], common[1]); 
         } else {
             answer = common[common.length-1] * findRhdql(common[0], common[1]); 
@@ -9,15 +9,15 @@ class Solution {
         return answer;
     }
 
-    public boolean isEmdck(int n1, int n2, int n3) { //등차수열 판별
+    public boolean check(int n1, int n2, int n3) {
         return (n1 + n3 == 2 * n2);
     }
 
-    public int findRhdck(int n1, int n2) { //공차 구하기
+    public int findRhdck(int n1, int n2) {
         return n2 - n1;
     }
 
-    public int findRhdql(int n1, int n2) { // 공비 구하기
+    public int findRhdql(int n1, int n2) {
         return n2 / n1;
     }
 }
