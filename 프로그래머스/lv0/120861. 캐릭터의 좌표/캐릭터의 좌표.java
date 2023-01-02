1138b160cc2dbb695c62c016b9fed036d9dcdf43
zhu <71738723+some1mine@users.noreply.github.com>
2023-01-02 17:54:18 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = new int[2]; answer[0] = board[0] / 2; answer[1] = board[1] / 2;
+        for(int i = 0 ; i < keyinput.length ; i++) {
+            if(keyinput[i].equals("left") && answer[0] > 0) answer[0]--;
+            if(keyinput[i].equals("right") && answer[0] < board[0] - 1) answer[0]++;
+            if(keyinput[i].equals("up") && answer[1] < board[1] - 1) answer[1]++;
+            if(keyinput[i].equals("down") && answer[1] > 0) answer[1]--;
+        }
+        answer[0] -= board[0] / 2;
+        answer[1] -= board[1] / 2;
+        return answer;
+    }
+}
