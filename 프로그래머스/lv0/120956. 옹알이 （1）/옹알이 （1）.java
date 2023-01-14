23702c44f70153b4bfd7d9cc4af3be6496db87b7
zhu <71738723+some1mine@users.noreply.github.com>
2023-01-15 02:05:53 +0900

    [level 0] Title: 옹알이 (1), Time: 0.06 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -3,23 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] can = {"aya", "ye", "woo", "ma"};
-        List<String> list = new ArrayList<>();
-        for(String s1 : can) {
-            list.add(s1);
-            for(String s2 : can) {
-                if(!s1.equals(s2)) list.add(s1 + s2);
-                for(String s3 : can) {
-                    if(!s1.equals(s2) && !s1.equals(s3) && !s2.equals(s3)) list.add(s1 + s2 + s3);
-                    for(String s4 : can) {
-                        if(!s1.equals(s2) && !s1.equals(s3) && !s1.equals(s4) && !s2.equals(s3)
-                          && !s2.equals(s4) && !s3.equals(s4)) list.add(s1 + s2 + s3 + s4);
-                    }
-                }
-            }
-        }
         for(String s : babbling) {
-            if(list.contains(s)) answer++;
+            s = s.replace("aya", "@");
+            s = s.replace("ye", "@");
+            s = s.replace("woo", "@");
+            s = s.replace("ma", "@");
+            s = s.replace("@", "");
+            if(s.length() == 0) answer++;
         }
         return answer;
     }
