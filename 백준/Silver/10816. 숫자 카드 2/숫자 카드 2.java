37da78e15df6fc85a678e76817a92711035b24b3
zhu <71738723+some1mine@users.noreply.github.com>
2023-01-15 00:41:56 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 888 ms, Memory: 198192 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,65 +1,24 @@
 import java.io.BufferedReader;
-
 import java.io.InputStreamReader;
-
-import java.io.IOException;
-
 import java.util.StringTokenizer;
 
- 
-
 public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        int n = Integer.parseInt(reader.readLine());
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        int[] arr = new int[20_000_001];
+        for(int i = 0 ; i < n ; i++) arr[Integer.parseInt(st.nextToken()) + 10_000_000]++;
+        
+        int m = Integer.parseInt(reader.readLine());
+        st = new StringTokenizer(reader.readLine());
 
- 
-
-	public static void main(String[] args) throws IOException {
-
-		
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-	
-
-		int[] counting = new int[20000001]; // 입력받는 수의 범위 : -10,000,000 ~ 10,000,000
-
-		
-
-		
-
-		int N = Integer.parseInt(br.readLine());
-
-		
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-		
-
-		for(int i = 0 ; i < N; i++) {
-
-			counting[Integer.parseInt(st.nextToken()) + 10000000]++;	// 해당 인덱스의 값 증가
-
-		}
-
-		
-
-		int M = Integer.parseInt(br.readLine());
-
-		
-
-		st = new StringTokenizer(br.readLine(), " ");
-
-		StringBuilder sb = new StringBuilder();
-
-		
-
-		for(int i = 0; i < M; i++) {
-
-			sb.append(counting[Integer.parseInt(st.nextToken()) + 10000000]).append(' ');
-
-		}
-
-		System.out.println(sb);
-
-	}
-
+        for(int i = 0 ; i < m ; i++) {
+            sb.append(arr[Integer.parseInt(st.nextToken()) + 10_000_000]);
+            if(i != m - 1) sb.append(" ");
+        }
+        System.out.println(sb.toString());
+        reader.close();
+    }
 }
