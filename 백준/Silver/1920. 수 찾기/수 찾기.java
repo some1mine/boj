41ac89d4204441198831111926a2a2799b3b4ed6
zhu <71738723+some1mine@users.noreply.github.com>
2023-01-14 23:54:10 +0900

    [Silver IV] Title: 수 찾기, Time: 560 ms, Memory: 42964 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
         int n = Integer.parseInt(reader.readLine());
         StringTokenizer st = new StringTokenizer(reader.readLine());
         int[] arr = new int[n];
@@ -11,22 +12,22 @@ public class Main {
         Arrays.sort(arr);
         int m = Integer.parseInt(reader.readLine());
         st = new StringTokenizer(reader.readLine());
-        int[] targets = new int[m];
-        for(int i = 0 ; i < m ; i++) targets[i] = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < m; i++) {
-            System.out.println(contains(arr, targets[i]));
+        for(int i = 0 ; i < m ; i++) {
+            if(contains(arr, Integer.parseInt(st.nextToken()))) sb.append(1).append("\n");
+            else sb.append(0).append("\n");
         }
+        System.out.println(sb.toString());
         reader.close();
     }
 
-    private static int contains(int[] arr, int target) {
+    private static boolean contains(int[] arr, int target) {
         int start = 0, end = arr.length - 1;
         while(start <= end) {
             int half = (start + end) / 2;
             if(arr[half] < target) start = half + 1;
             if(arr[half] > target) end = half - 1;
-            if(arr[half] == target) return 1;
+            if(arr[half] == target) return true;
         }
-        return 0;
+        return false;
     }
 }
