a3f31cf473873b665613bd365e93e74d36fe6a97
zhu <71738723+some1mine@users.noreply.github.com>
2023-02-14 17:29:45 +0900

    [Gold IV] Title: 검문, Time: 1440 ms, Memory: 15868 KB -BaekjoonHub

---
+++ 백준/Gold/2981. 검문/검문.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++) arr[i] = Integer.parseInt(reader.readLine());
+        Arrays.sort(arr);
+        int gcd = arr[1] - arr[0];
+        for(int i = 2; i < n ; i++) gcd = gcd(gcd, arr[i] - arr[i - 1]);
+        for(int i = 2; i <= gcd; i++) {
+            if(gcd % i == 0) System.out.print(i + " ");
+        }
+    }
+    public static int gcd(int num1, int num2) {
+        while(num2 != 0) {
+            int r = num1 % num2;
+            num1 = num2;
+            num2 = r;
+        }
+        return num1;
+    }
+}
