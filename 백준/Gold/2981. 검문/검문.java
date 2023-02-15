cb89756b2f6a5c21a63940d43f1baf1393acbd66
zhu <71738723+some1mine@users.noreply.github.com>
2023-02-15 22:17:33 +0900

    [Gold IV] Title: 검문, Time: 1424 ms, Memory: 16064 KB -BaekjoonHub

--- 백준/Gold/2981. 검문/검문.java
+++ 백준/Gold/2981. 검문/검문.java
@@ -1,5 +1,5 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 import java.util.stream.*;
 
 public class Main {
@@ -10,13 +10,15 @@ public class Main {
         for(int i = 0 ; i < n ; i++) arr[i] = Integer.parseInt(reader.readLine());
         Arrays.sort(arr);
         int gcd = arr[1] - arr[0];
-        for(int i = 2; i < n ; i++) gcd = gcd(gcd, arr[i] - arr[i - 1]);
-        for(int i = 2; i <= gcd; i++) {
+        for(int i = 2; i < n ; i++) {
+            gcd = getGcd(arr[i] - arr[i - 1], gcd);
+        }
+        for(int i = 2 ; i <= gcd; i++) {
             if(gcd % i == 0) System.out.print(i + " ");
         }
     }
-    public static int gcd(int num1, int num2) {
-        while(num2 != 0) {
+    public static int getGcd(int num1, int num2) {
+        while(num2 > 0) {
             int r = num1 % num2;
             num1 = num2;
             num2 = r;
