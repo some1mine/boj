3c6057fdcc1f6a6518b8e17867d46031e6e7fb44
zhu <71738723+some1mine@users.noreply.github.com>
2023-03-15 05:26:28 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n, int a, int b) {
+        int answer = 1;
+        while((a + 1) / 2 != (b + 1) / 2 && n > 0) {
+            n /= 2;
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            answer++;
+        }
+
+        return answer;
+    }
+}
