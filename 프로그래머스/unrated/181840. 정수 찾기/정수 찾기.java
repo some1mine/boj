817c5ee926f040f2b46160eb68b1d5646a3755f6
zhu <71738723+some1mine@users.noreply.github.com>
2023-04-26 22:17:39 +0900

    [unrated] Title: 정수 찾기, Time: 1.93 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+        int answer = Arrays.stream(num_list).anyMatch(x -> x == n) ? 1 : 0;
+        return answer;
+    }
+}
