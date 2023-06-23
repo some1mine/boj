c91cd2e3d186c349be44cc0eefd92734eaafc9f5
zhu <71738723+some1mine@users.noreply.github.com>
2023-06-23 23:44:39 +0900

    [Silver II] Title: 색종이 만들기, Time: 196 ms, Memory: 16716 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,35 +1,37 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
     public static int white = 0, blue = 0;
-    public static int[][] arr;
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
-        arr = new int[n][n];
-        for(int i = 0 ; i < n ; i++)
-            arr[i] = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
-        divide(0, 0, n);
-        System.out.println(white + "\n" + blue);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        int[][] arr =  new int[n][n];
+        for(int i = 0 ; i < n ; i++) arr[i] = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        solution(arr, n, 0, 0);
+        System.out.print(white + "\n" + blue);
     }
-    public static void divide(int num1, int num2, int n) {
-        if(isFlatColor(num1, num2, n)) {
-            if(arr[num1][num2] == 0) white++;
+    public static void solution(int[][] arr, int length, int startX, int startY) {
+        if(isEven(arr, length, startX, startY)) {
+            if(arr[startY][startX] == 0) white++;
             else blue++;
             return;
         }
-        divide(num1, num2, n / 2);
-        divide(num1, num2 + n / 2, n / 2);
-        divide(num1 + n / 2, num2, n / 2);
-        divide(num1 + n / 2, num2 + n / 2, n / 2);
+        for(int i = startY ; i < startY + length ; i += length) {
+            for(int j = startX ; j < startX + length ; j += length) {
+                if(!isEven(arr, length, startX, startY)) {
+                    solution(arr, length / 2, startX, startY);
+                    solution(arr, length / 2, startX + length / 2, startY);
+                    solution(arr, length / 2, startX, startY + length / 2);
+                    solution(arr, length / 2, startX + length / 2, startY + length / 2);
+                }
+            }
+        }
     }
-
-    public static boolean isFlatColor(int num1, int num2, int n) {
-        int color = arr[num1][num2];
-        for(int i = num1 ; i < num1 + n ; i++) {
-            for(int j = num2 ; j < num2 + n ; j++) {
-                if(arr[i][j] != color) return false;
+    public static boolean isEven(int[][] arr, int length, int startX, int startY) {
+        for(int i = startY ; i < startY + length ; i++) {
+            for(int j = startX ; j < startX + length ; j++) {
+                if(arr[i][j] != arr[startY][startX]) return false;
             }
         }
         return true;
