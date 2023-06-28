c44efb9e9fd535590e9b16c11d7e87f4d0d99b6d
zhu <71738723+some1mine@users.noreply.github.com>
2023-06-28 22:40:29 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 16.27 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,34 +1,19 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] arr = s.split(" ");
-        int cnt = 0;
-        for(int i = 0 ; i < arr.length ; i++){
-            for(int j = 0 ; j < arr[i].length() ; j++){
-                if(j % 2 == 0){
-                    if(arr[i].charAt(j) < 97){
-                        answer += Character.toString(arr[i].charAt(j));
-                    } else{
-                        answer += Character.toString(arr[i].charAt(j) - 32);
-                    }
-                } else{
-                    if(arr[i].charAt(j) >= 97){
-                        answer += Character.toString(arr[i].charAt(j));
-                    } else{
-                        answer += Character.toString(arr[i].charAt(j) + 32);
-                    }
+        boolean toUpper = true;
+        for(char c : s.toCharArray()) {
+            if(!Character.isAlphabetic(c)) {
+                answer += c; toUpper = true;
+            } else {
+                if(toUpper) {
+                    answer += Character.toUpperCase(c);
+                } else {
+                    answer += Character.toLowerCase(c);
                 }
+                toUpper = !toUpper;
             }
-            answer += " ";
         }
-        answer = answer.trim();
-        if(s.charAt(0) == ' '){
-            answer = " " + answer;
-        }
-        if(s.charAt(s.length()-1) == ' ') {
-            answer += " ";
-        }
-        if(s.length() > answer.length()) answer += s.substring(answer.length());
         return answer;
     }
 }
