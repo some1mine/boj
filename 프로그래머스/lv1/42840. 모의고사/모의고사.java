a4961efc5b18213097b648589413123fd1ac50e8
Jaeu <71738723+some1mine@users.noreply.github.com>
2023-07-04 06:31:02 +0900

    [level 1] Title: 모의고사, Time: 4.98 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,25 +1,18 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] one = {1, 2, 3, 4, 5};
-        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int count1 = 0, count2 = 0, count3 = 0;
+        int[][] ways = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+        int[] count = new int[3];
+        int max = -1;
         for(int i = 0 ; i < answers.length ; i++) {
-            if(answers[i] == one[i % one.length]) count1++;
-            if(answers[i] == two[i % two.length]) count2++;
-            if(answers[i] == three[i % three.length]) count3++;
+            for(int j = 0 ; j < ways.length ; j++) {
+                if(answers[i] == ways[j][i % ways[j].length]) count[j]++;
+                max = Math.max(max, count[j]);
+            }
         }
-        if(count1 < Math.max(count2, count3)) {
-            if(count2 > count3) return new int[]{2};
-            if(count2 == count3) return new int[]{2, 3};
-            return new int[]{3};
-        } else {
-            if(count2 == count1 && count3 == count1) return new int[]{1, 2, 3};
-            if(count1 == count2) return new int[]{1, 2};
-            if(count1 == count3) return new int[]{1, 3};
-            return new int[]{1};
-        }
+        final int maxNum = max;
+        return IntStream.range(0, 3).filter(i -> count[i] == maxNum).map(i -> i + 1).toArray();
     }
 }
