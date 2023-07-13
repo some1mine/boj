f007350751334e00071ff5bbaf553484b09c2e45
박재우 <71738723+some1mine@users.noreply.github.com>
2023-07-13 14:26:44 +0900

    [Bronze V] Title: A+B, Time: 120 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(input.charAt(0) + input.charAt(2) - 96);
+    }
+}
