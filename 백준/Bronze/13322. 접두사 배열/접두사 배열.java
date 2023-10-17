aac0f1c3daf71aace221d9095afffcebdba86a35
박재우 <71738723+some1mine@users.noreply.github.com>
2023-10-17 21:25:20 +0900

    [Bronze I] Title: 접두사 배열, Time: 1232 ms, Memory: 35096 KB -BaekjoonHub

---
+++ 백준/Bronze/13322. 접두사 배열/접두사 배열.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        for(int i = 0 ; i < input.length() ; i++) System.out.println(i);
+    }
+}
