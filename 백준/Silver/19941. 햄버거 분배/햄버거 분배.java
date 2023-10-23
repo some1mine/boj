922d6c2049fbb109bd4ca8e122c9b60da12639a4
박재우 <71738723+some1mine@users.noreply.github.com>
2023-10-23 23:45:56 +0900

    [Silver III] Title: 햄버거 분배, Time: 288 ms, Memory: 19080 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -5,28 +5,33 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int[] nk = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         char[] input = scanner.nextLine().toCharArray();
-        int answer = 0; boolean mode = true;
+        int answer = 0;
         if(input[0] == 'H') {
             for(int i = 0 ; i < nk[0] ; i++) {
-                if(input[i] == 'P' && mode) {
-                    for(int j = Math.max(0, i - nk[1]) ; j <= Math.min(nk[0] - 1, i + nk[1]) ; j++) {
-                        if(input[j] == 'H') {
-                            input[j] = 'X'; answer++; break;
-                        }
-                    }
-                }
+                if (input[i] == 'P' && isPossible(nk, input, i, true)) answer++;
             }
         } else {
             for(int i = nk[0] - 1 ; i >= 0 ; i--) {
-                if(input[i] == 'P') {
-                    for(int j = Math.min(nk[0] - 1, i + nk[1]) ; j >= Math.max(0, i - nk[1]) ; j--) {
-                        if(input[j] == 'H') {
-                            input[j] = 'X'; answer++; break;
-                        }
-                    }
-                }
+                if(input[i] == 'P' && isPossible(nk, input, i, false)) answer++;
             }
         }
         System.out.print(answer);
+    }
+
+    private static boolean isPossible(int[] nk, char[] input, int i, boolean mode) {
+        if (mode) {
+            for(int j = Math.max(0, i - nk[1]); j <= Math.min(nk[0] - 1, i + nk[1]) ; j++) {
+                if (input[j] == 'H') {
+                    input[j] = 'X'; return true;
+                }
+            }
+        } else {
+            for(int j = Math.min(nk[0] - 1, i + nk[1]) ; j >= Math.max(0, i - nk[1]) ; j--) {
+                if(input[j] == 'H') {
+                    input[j] = 'X'; return true;
+                }
+            }
+        }
+        return false;
     }
 }
