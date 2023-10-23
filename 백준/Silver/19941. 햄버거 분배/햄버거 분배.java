892f67f596950e0dc3b958cf177b69a9e1efe286
박재우 <71738723+some1mine@users.noreply.github.com>
2023-10-23 23:47:33 +0900

    [Silver III] Title: 햄버거 분배, Time: 288 ms, Memory: 18992 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -8,30 +8,26 @@ public class Main {
         int answer = 0;
         if(input[0] == 'H') {
             for(int i = 0 ; i < nk[0] ; i++) {
-                if (input[i] == 'P' && isPossible(nk, input, i, true)) answer++;
+                if (input[i] == 'P' && isPossible(nk, input, i, true) == 'X') answer++;
             }
         } else {
             for(int i = nk[0] - 1 ; i >= 0 ; i--) {
-                if(input[i] == 'P' && isPossible(nk, input, i, false)) answer++;
+                if(input[i] == 'P' && isPossible(nk, input, i, false) == 'X') answer++;
             }
         }
         System.out.print(answer);
     }
 
-    private static boolean isPossible(int[] nk, char[] input, int i, boolean mode) {
+    private static char isPossible(int[] nk, char[] input, int i, boolean mode) {
         if (mode) {
             for(int j = Math.max(0, i - nk[1]); j <= Math.min(nk[0] - 1, i + nk[1]) ; j++) {
-                if (input[j] == 'H') {
-                    input[j] = 'X'; return true;
-                }
+                if (input[j] == 'H') return input[j] = 'X';
             }
         } else {
             for(int j = Math.min(nk[0] - 1, i + nk[1]) ; j >= Math.max(0, i - nk[1]) ; j--) {
-                if(input[j] == 'H') {
-                    input[j] = 'X'; return true;
-                }
+                if(input[j] == 'H') return input[j] = 'X';
             }
         }
-        return false;
+        return ' ';
     }
 }
