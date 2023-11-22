e4fda51420ab74565fe5b15a20dc31a44e307692
박재우 <71738723+some1mine@users.noreply.github.com>
2023-11-22 20:06:16 +0900

    [Silver V] Title: 배열 합치기, Time: 3048 ms, Memory: 393660 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        reader.readLine();
+        PriorityQueue<Integer> queue1 = new PriorityQueue<>();
+        PriorityQueue<Integer> queue2 = new PriorityQueue<>();
+        Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).forEach(queue1::add);
+        Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).forEach(queue2::add);
+        while(!queue1.isEmpty() && !queue2.isEmpty()) {
+            if(queue1.peek() <= queue2.peek()) writer.write(String.valueOf(queue1.poll()));
+            else writer.write(String.valueOf(queue2.poll()));
+            writer.write(" ");
+        }
+        while(!queue1.isEmpty()) writer.write(queue1.poll() + " ");
+        while(!queue2.isEmpty()) writer.write(queue2.poll() + " ");
+        writer.flush(); reader.close(); writer.close();
+    }
+}
