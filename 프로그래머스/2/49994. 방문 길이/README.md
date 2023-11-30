6d6dafeb80005bda7468a0370f8dc83085821afc
박재우 <71738723+some1mine@users.noreply.github.com>
2023-12-01 05:16:53 +0900

    [level 2] Title: 방문 길이, Time: 4.01 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/README.md
+++ 프로그래머스/2/49994. 방문 길이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 방문 길이 - 49994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994?language=java) 
 
 ### 성능 요약
 
-메모리: 83.3 MB, 시간: 3.02 ms
+메모리: 77 MB, 시간: 4.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 5:2:50
+2023년 12월 5일 5:16:48
 
 ### 문제 설명
 
--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,17 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(String dirs) {
+        int answer = 0, y = 0, x = 0;
         Set<Map<Set<Integer>, Set<Integer>>> set = new HashSet<>(); 
         Map<Set<Integer>, Set<Integer>> move = null; 
-        int answer = 0, y = 0, x = 0;
         for(char c : dirs.toCharArray()) {
             if(c == 'U' && y != 5) move = Map.of(Set.of(y, ++y), Set.of(x));
-            if(c == 'R' && x != 5) move = Map.of(Set.of(y), Set.of(x, ++x)); 
             if(c == 'D' && y != -5) move = Map.of(Set.of(y, --y), Set.of(x));
-            if(c == 'L' && x != -5) move = Map.of(Set.of(y), Set.of(x, --x));  
-            if(!set.contains(move)) {
-                set.add(move); answer++; 
-            }
+            if(c == 'R' && x != 5) move = Map.of(Set.of(y), Set.of(x, ++x));
+            if(c == 'L' && x != -5) move = Map.of(Set.of(y), Set.of(x, --x));
+            if(set.contains(move)) continue;
+            set.add(move); answer++;
         }
         return answer;
     }
