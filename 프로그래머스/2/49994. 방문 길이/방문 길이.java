2b249c60197fef8afa1b3bd30e51902ab25f7d6a
박재우 <71738723+some1mine@users.noreply.github.com>
2023-12-01 04:58:26 +0900

    [level 2] Title: 방문 길이, Time: 1.85 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -4,10 +4,10 @@ class Solution {
     public int solution(String dirs) {
         Set<Direction> set = new HashSet<>(); Direction move = null; int answer = 0, y = 0, x = 0;
         for(char c : dirs.toCharArray()) {
-            if(c == 'U' && y != 5) move = new Direction(y, ++y, x, x);
-            if(c == 'R' && x != 5) move = new Direction(y, y, x, ++x); 
-            if(c == 'D' && y != -5) move = new Direction(y, --y, x, x);
-            if(c == 'L' && x != -5) move = new Direction(y, y, x, --x);  
+            if(c == 'U' && y != 5) move = new Direction(Set.of(y, ++y), Set.of(x));
+            if(c == 'R' && x != 5) move = new Direction(Set.of(y), Set.of(x, ++x)); 
+            if(c == 'D' && y != -5) move = new Direction(Set.of(y, --y), Set.of(x));
+            if(c == 'L' && x != -5) move = new Direction(Set.of(y), Set.of(x, --x));  
             if(!set.contains(move)) {
                 set.add(move); answer++;
             }
@@ -17,9 +17,9 @@ class Solution {
 }
 
 class Direction {
-    Set<Integer> ySet = new HashSet<>(), xSet = new HashSet<>();
-    public Direction(int fromY, int toY, int fromX, int toX) {
-        ySet.add(fromY); ySet.add(toY); xSet.add(fromX); xSet.add(toX);
+    Set<Integer> ySet, xSet;
+    public Direction(Set<Integer> ySet, Set<Integer> xSet) {
+        this.ySet = ySet; this.xSet = xSet;
     }
     @Override
     public boolean equals(Object o) {
