2b2770890002affe68bd37667524212d872f78ab
박재우 <71738723+some1mine@users.noreply.github.com>
2023-12-01 05:28:44 +0900

    [level 2] Title: 방문 길이, Time: 3.30 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(String dirs) {
         int answer = 0, y = 0, x = 0;
-        Map move = null; Set set = new HashSet<>();
+        Set set = new HashSet<>(); Map move = null;
         for(char c : dirs.toCharArray()) {
             if(c == 'U' && y != 5) move = Map.of(Set.of(y, ++y), Set.of(x));
             if(c == 'D' && y != -5) move = Map.of(Set.of(y, --y), Set.of(x));
