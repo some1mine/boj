81eaed815221fd98fd9cbd6d94554389f042d3ce
박재우 <71738723+some1mine@users.noreply.github.com>
2023-12-01 05:29:34 +0900

    [level 2] Title: 방문 길이, Time: 3.17 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -6,8 +6,8 @@ class Solution {
         Set set = new HashSet<>(); Map move = null;
         for(char c : dirs.toCharArray()) {
             if(c == 'U' && y != 5) move = Map.of(Set.of(y, ++y), Set.of(x));
-            if(c == 'D' && y != -5) move = Map.of(Set.of(y, --y), Set.of(x));
             if(c == 'R' && x != 5) move = Map.of(Set.of(y), Set.of(x, ++x));
+            if(c == 'D' && y != -5) move = Map.of(Set.of(y, --y), Set.of(x));
             if(c == 'L' && x != -5) move = Map.of(Set.of(y), Set.of(x, --x));
             if(set.contains(move)) continue;
             set.add(move); answer++;
