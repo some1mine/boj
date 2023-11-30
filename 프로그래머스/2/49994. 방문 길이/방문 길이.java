4b92d434e297649cbb06b06c48878907666c475a
박재우 <71738723+some1mine@users.noreply.github.com>
2023-12-01 06:02:06 +0900

    [level 2] Title: 방문 길이, Time: 3.25 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(String dirs) {
-        int answer = 0, y = 0, x = 0;
+        int answer = 0, x = 0, y = 0;
         Set set = new HashSet<>();
         for(char c : dirs.toCharArray()) {
             Map move =
-                    (c == 'U' && y <  5) ? Map.of(Set.of(y, ++y), Set.of(x)) : // 상
-                    (c == 'D' && y > -5) ? Map.of(Set.of(y, --y), Set.of(x)) : // 하
-                    (c == 'L' && x > -5) ? Map.of(Set.of(y), Set.of(x, --x)) : // 좌
-                    (c == 'R' && x <  5) ? Map.of(Set.of(y), Set.of(x, ++x)) : // 우
+                    (c == 'U' && y <  5) ? Map.of(Set.of(x), Set.of(y, ++y)) : // 상
+                    (c == 'D' && y > -5) ? Map.of(Set.of(x), Set.of(y, --y)) : // 하
+                    (c == 'L' && x > -5) ? Map.of(Set.of(x, --x), Set.of(y)) : // 좌
+                    (c == 'R' && x <  5) ? Map.of(Set.of(x, ++x), Set.of(y)) : // 우
                     null;
             if(move == null || set.contains(move)) continue;
             set.add(move); answer++;
