e2c9b84f9f4ea8070335c45a6ed70e4f51900736
박재우 <71738723+some1mine@users.noreply.github.com>
2023-12-01 05:44:19 +0900

    [level 2] Title: 방문 길이, Time: 3.69 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -5,10 +5,11 @@ class Solution {
         int answer = 0, y = 0, x = 0;
         Set set = new HashSet<>();
         for(char c : dirs.toCharArray()) {
-            Map move =  c == 'U' && y != 5 ? move = Map.of(Set.of(y, ++y), Set.of(x)) :
-                        c == 'R' && x != 5 ? move = Map.of(Set.of(y), Set.of(x, ++x)) :
-                        c == 'D' && y != -5 ? move = Map.of(Set.of(y, --y), Set.of(x)) :
-                        c == 'L' && x != -5 ? move = Map.of(Set.of(y), Set.of(x, --x)) : null;
+            Map move =
+                    c == 'U' && y != 5 ? move = Map.of(Set.of(y, ++y), Set.of(x)) :
+                    c == 'R' && x != 5 ? move = Map.of(Set.of(y), Set.of(x, ++x)) :
+                    c == 'D' && y != -5 ? move = Map.of(Set.of(y, --y), Set.of(x)) :
+                    c == 'L' && x != -5 ? move = Map.of(Set.of(y), Set.of(x, --x)) : null;
             if(move == null || set.contains(move)) continue;
             set.add(move); answer++;
         }
