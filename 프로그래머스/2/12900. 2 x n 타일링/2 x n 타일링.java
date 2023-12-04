fd76f57d7b1bbbc90be239b65d02274333cb8612
박재우 <71738723+some1mine@users.noreply.github.com>
2023-12-05 01:30:35 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.23 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int n) {
-        int[] dp = new int[n + 1]; dp[1] = 1; dp[2] = 2;
-        for(int i = 3 ; i <= n ; i++) dp[i] = (dp[i - 1] + dp[i - 2]) % 1_000_000_007;
+        int[] dp = new int[n + 1]; dp[0] = 1; dp[1] = 1;
+        for(int i = 2 ; i <= n ; i++) dp[i] = (dp[i - 1] + dp[i - 2]) % 1_000_000_007;
         return dp[n];
     }
 }
