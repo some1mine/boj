e653d788b2a36b48821400dc68a1b676be61fee5
박재우 <71738723+some1mine@users.noreply.github.com>
2023-12-05 03:39:34 +0900

    [level 2] Title: 괄호 변환, Time: 8.18 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String solution(String p) {
+        return get(p, 0);
+    }
+    public String get(String p, int flag) {
+        if(p.length() == 0) return p;
+        for(int i = 0 ; i < p.length() ; i++) {
+            flag = p.charAt(i) == '(' ? flag + 1 : flag - 1;
+            if(flag == 0) {
+                p = recursion(p, i + 1); break;
+            }
+        }
+        return p;
+    }
+    public String recursion(String s, int idx) {
+        String u = s.substring(0, idx), v = s.substring(idx);
+        if(!isProper(u, 0)) return "(" + get(v, 0) + ")" + reverse(u.substring(1, u.length() - 1));
+        return u + get(v, 0);
+    }
+    public boolean isProper(String s, int flag) {
+        for(int i = 0 ; i < s.length() ; i++) {
+            flag = s.charAt(i) == '(' ? flag + 1 : flag - 1;
+            if(flag < 0) return false;
+        }
+        return true;
+    }
+    public String reverse(String s) {
+        char[] arr = s.toCharArray();
+        for(int i = 0 ; i < arr.length ; i++) {
+            arr[i] = arr[i] == ')' ? '(' : ')';
+        }
+        return new String(arr);
+    }
+}
