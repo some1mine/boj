b45b2a82344ebb1d01d1680c565babca9dc27114
박재우 <71738723+some1mine@users.noreply.github.com>
2023-12-20 16:30:54 +0900

    [level 2] Title: 택배상자, Time: 132.66 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,23 +2,18 @@ import java.util.*;
 import java.util.stream.*;
 
 class Solution {
+    public int answer = 0, idx = 0;
+    public Stack<Integer> sub = new Stack<>();
     public int solution(int[] order) {
-        int answer = 0, idx = 0;
-        int[] arr = IntStream.rangeClosed(1, order.length).toArray();
-        Stack<Integer> sub = new Stack<>();
         for(int i = 0 ; i < order.length ; i++) {
-            if(order[idx] != i + 1) {
-                if(idx < order.length && (sub.isEmpty() || sub.peek() != order[idx])) sub.push(i + 1);
-                while(!sub.isEmpty() && idx < order.length && sub.peek() == order[idx]) {
-                    sub.pop(); answer++; idx++;
-                }
-            } else {
-                idx++; answer++;
-                while(!sub.isEmpty() && idx < order.length && sub.peek() == order[idx]) {
-                    sub.pop(); answer++; idx++;
-                }
-            }
+            sub.push(i + 1);
+            extractFromSub(order);
         }
         return answer;
+    }
+    public void extractFromSub(int[] order) {
+        while(!sub.isEmpty() && idx < order.length && sub.peek() == order[idx]) {
+            sub.pop(); answer++; idx++;
+        }
     }
 }
