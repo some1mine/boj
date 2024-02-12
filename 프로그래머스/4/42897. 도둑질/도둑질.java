dfdb8989f8b70b5c5dc1356dc7a3e2c315309dee
박재우 <71738723+some1mine@users.noreply.github.com>
2024-02-13 02:18:44 +0900

    [level 4] Title: 도둑질, Time: 32.49 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] money) {        
+        if(money.length == 3) return Math.max(money[2], Math.max(money[0], money[1]));
+        
+        int[] dp1 = new int[money.length - 1]; dp1[0] = money[0]; dp1[1] = money[1];
+        int[] dp2 = new int[money.length]; dp2[1] = money[1];
+        
+        for(int i = 2 ; i < money.length - 1 ; i++) {
+            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + money[i]);
+            if(i > 2) dp1[i] = Math.max(dp1[i], dp1[i - 3] + money[i]);
+        }
+        for(int i = 2 ; i < money.length ; i++) {
+            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + money[i]);
+            if(i > 2) dp2[i] = Math.max(dp2[i], dp2[i - 3] + money[i]);
+        }
+        return Math.max(dp1[money.length - 2], dp2[money.length - 1]);
+    }
+}
