056a9a8332e3a7028d8efea2ac28f10eccf2d7e4
박재우 <71738723+some1mine@users.noreply.github.com>
2024-02-20 00:56:28 +0900

    [Silver V] Title: 다리 놓기, Time: 180 ms, Memory: 15332 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,26 +1,30 @@
-import java.math.BigInteger;
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
+import java.math.*;
+/**
+nCm
+*/
 public class Main {
+    public static Map<Integer, BigInteger> map = new HashMap<>();
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
-        int n = Integer.parseInt(reader.readLine());
-        for(int i = 0 ; i < n ; i++) {
-            int[] nm = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            sb.append(solution(nm[0], nm[1])).append("\n");
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(reader.readLine());
+        initializeMap();
+        while(T-- > 0) {
+            int[] ab = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            sb.append(count(ab[0], ab[1]).toString()).append("\n");
         }
-        System.out.println(sb);
+        reader.close(); System.out.print(sb);
     }
-
-    private static int solution(int n, int m) {
-        int max = Math.max(n, m), min = Math.min(n, m);
-        return (factorial(max).divide(factorial(max - min)).divide(factorial(min))).intValue();
+    public static void initializeMap() {
+        BigInteger n = new BigInteger("1"); map.put(0, n);
+        for(int i = 1 ; i < 30 ; i++) map.put(i, n = map.get(i - 1).multiply(new BigInteger(i + "")));
     }
-    private static BigInteger factorial(int n) {
-        BigInteger answer = BigInteger.ONE;
-        for(int i = 1; i <= n ; i++) answer = answer.multiply(BigInteger.valueOf(i));
-        return answer;
+    public static BigInteger count(int min, int max) {
+        return factorial(max).divide(factorial(min)).divide(factorial(max - min));
+    }
+    public static BigInteger factorial(int n) {
+        return map.get(n);
     }
 }
