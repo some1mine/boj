5bea85cdf344126dd8ac914d2824b93c9d774182
박재우 <71738723+some1mine@users.noreply.github.com>
2024-02-21 23:48:18 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 2.29 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arrayA, int[] arrayB) {
+        int answer = 0; List<Integer> gcdA = getGcd(arrayA, arrayB), gcdB = getGcd(arrayB, arrayA);
+        int maxA = gcdA.stream().filter(a -> !gcdB.contains(a)).mapToInt(i -> i).max().orElse(0),
+            maxB = gcdB.stream().filter(b -> !gcdA.contains(b)).mapToInt(i -> i).max().orElse(0);
+        return Math.max(maxA, maxB);
+    }
+    public List<Integer> getGcd(int[] arr1, int[] arr2) {
+        List<Integer> list = new ArrayList<>();
+        int min = Arrays.stream(arr1).min().getAsInt();
+        List<Integer> dividors = get(min);
+        out: for(int d : dividors) {
+            for(int a : arr1) {
+                if(a % d != 0) continue out;
+            }
+            for(int a : arr2) {
+                if(a % d == 0) continue out;
+            }
+            list.add(d);
+        }
+        return list;
+    }
+    public List<Integer> get(int n) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1 ; i <= n ; i++) {
+            if(n % i == 0) list.add(i);
+        }
+        return list;
+    }
+}
