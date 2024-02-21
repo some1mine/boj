ee41a6c9a35207b0c48bcb5d9e2ef1e24147d791
박재우 <71738723+some1mine@users.noreply.github.com>
2024-02-21 23:55:13 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 9.41 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
@@ -7,25 +8,13 @@ class Solution {
             gcdB.stream().filter(b -> !gcdA.contains(b)).mapToInt(i -> i).max().orElse(0));
     }
     public List<Integer> getGcd(int[] arr1, int[] arr2) {
-        List<Integer> list = new ArrayList<>();
         int min = Arrays.stream(arr1).min().getAsInt();
         List<Integer> dividors = get(min);
-        out: for(int d : dividors) {
-            for(int a : arr1) {
-                if(a % d != 0) continue out;
-            }
-            for(int a : arr2) {
-                if(a % d == 0) continue out;
-            }
-            list.add(d);
-        }
-        return list;
+        return dividors.stream().filter(d -> 
+            Arrays.stream(arr1).noneMatch(a -> a % d > 0) && Arrays.stream(arr2).noneMatch(a -> a % d == 0)
+        ).collect(Collectors.toList());
     }
     public List<Integer> get(int n) {
-        List<Integer> list = new ArrayList<>();
-        for(int i = 1 ; i <= n ; i++) {
-            if(n % i == 0) list.add(i);
-        }
-        return list;
+        return IntStream.rangeClosed(1, n).filter(i -> n % i == 0).boxed().collect(Collectors.toList());
     }
 }
