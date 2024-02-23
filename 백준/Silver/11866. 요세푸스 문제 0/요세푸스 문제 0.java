c2d5cf8dda97488f5e605768609e512844e37a51
박재우 <71738723+some1mine@users.noreply.github.com>
2024-02-23 17:28:56 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 132 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,30 +1,19 @@
 import java.io.*;
 import java.util.*;
-
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] in = br.readLine().split(" ");
-        int[] arr = {Integer.parseInt(in[0]), Integer.parseInt(in[1])};
-        
-        String result = "<";
-        int ord = 0;
-        
-        LinkedList<Integer> list = new LinkedList<>();
-        
-        for(int i = 0 ; i < arr[0] ; i++) list.add(i + 1);
-        
-        System.out.print("<");
-        
-        while(list.size() != 0) {
-            for(int i = 0 ; i < arr[1] ; i++) {
-                if(i == arr[1] - 1) {
-                    if(list.size() == 1) System.out.print(list.remove());
-                    else System.out.print(list.remove() + ", ");
-                } else list.add(list.remove());
-            }
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] nk = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        StringBuilder sb = new StringBuilder(); sb.append("<"); int ord = nk[1] - 1;
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0 ; i < nk[0] ; i++) list.add(i + 1);
+        while(true) {
+            sb.append(list.remove(ord--));
+            if(list.isEmpty()) break;
+            sb.append(", ");
+            ord = (ord + nk[1]) % list.size();
         }
-        System.out.print(">");
+        System.out.print(sb); System.out.print(">");
     }
 }
