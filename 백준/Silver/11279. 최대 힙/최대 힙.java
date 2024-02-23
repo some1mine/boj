9e8476cceba1eefecdd55f0b7bccd16605c6c28c
박재우 <71738723+some1mine@users.noreply.github.com>
2024-02-23 17:37:52 +0900

    [Silver II] Title: 최대 힙, Time: 332 ms, Memory: 26540 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,20 +1,19 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        StringBuffer sb = new StringBuffer();
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-        int n = scanner.nextInt();
-        for (int i = 0; i < n; i++) {
-            int ord = scanner.nextInt();
-            if(ord == 0 && queue.isEmpty()) sb.append(0).append("\n");
-            else if(ord == 0) sb.append(queue.poll()).append("\n");
-            else queue.add(ord);
+        int n = Integer.parseInt(reader.readLine());
+        StringBuilder sb = new StringBuilder();
+        while(n-- >0) {
+            int input = Integer.parseInt(reader.readLine());
+            if(input == 0 && queue.isEmpty()) sb.append(0).append("\n");
+            else if(input == 0) sb.append(queue.poll()).append("\n");
+            else queue.offer(input);
         }
-        System.out.println(sb);
-        scanner.close();
+        System.out.print(sb);
     }
 }
