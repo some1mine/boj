cd7e894c0f5a9d08f222a23392db0e1fe9b2ffed
박재우 <71738723+some1mine@users.noreply.github.com>
2024-02-23 17:45:07 +0900

    [Silver V] Title: 영화감독 숌, Time: 284 ms, Memory: 85020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,19 +1,14 @@
-import java.util.Scanner;
-
-public class Main{
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int cnt = 0;
-        int i;
-        for(i=666;i<100000000;i++){
-            if((i+"").contains("666")){
-                cnt++;
-                }
-            if(cnt == a) {
-                break;  
-                }
-         }
-     System.out.println(i);
-  }
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine()), ord = 0;
+        int[] arr = new int[n];
+        for(int i = 0 ; i < 1_000_000_000 && ord < n ; i++) {
+            if(Integer.toString(i).contains("666")) arr[ord++] = i;
+        }
+        System.out.print(arr[n - 1]);
+    }
 }
