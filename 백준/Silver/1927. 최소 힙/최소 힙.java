8985c45898b34585d844553ed3b33b7394d97ab2
박재우 <71738723+some1mine@users.noreply.github.com>
2024-02-23 17:39:56 +0900

    [Silver II] Title: 최소 힙, Time: 332 ms, Memory: 26400 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,19 +1,19 @@
+import java.io.*;
 import java.util.*;
-
+ 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        StringBuffer sb = new StringBuffer();
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
         PriorityQueue<Integer> queue = new PriorityQueue<>();
-        int n = scanner.nextInt();
-        for (int i = 0; i < n; i++) {
-            int ord = scanner.nextInt();
-            if(ord == 0 && queue.isEmpty()) sb.append(0).append("\n");
-            else if(ord == 0) sb.append(queue.poll()).append("\n");
-            else queue.add(ord);
+        StringBuilder sb = new StringBuilder();
+        while(n-- > 0) {
+            int x = Integer.parseInt(reader.readLine());
+            if(x == 0) {
+                sb.append(queue.isEmpty() ? 0 : queue.poll()).append("\n"); continue;
+            }
+            queue.offer(x);
         }
         System.out.println(sb);
-        scanner.close();
     }
 }
-    
