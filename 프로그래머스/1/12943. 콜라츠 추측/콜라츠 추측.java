7ace3b5fbdc89f61a6da9f7e875fe20f75f8ce59
박재우 <71738723+some1mine@users.noreply.github.com>
2024-02-25 16:48:54 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -2,6 +2,6 @@ class Solution {
     public int solution(long num) {
         int i;
         for(i = 0 ; i < 500 && num != 1 ; i++) num = num % 2 == 0 ? num / 2 : num * 3 + 1;
-        return num != 1 ? -1 : i;
+        return num == 1 ? i : -1;
     }
 }
