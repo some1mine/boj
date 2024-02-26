d9b682b5625ec5b238e45282e588810b60e98b2c
박재우 <71738723+some1mine@users.noreply.github.com>
2024-02-27 00:07:46 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 4.74 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -8,22 +8,22 @@ class Solution {
              diff = cntY - cntX, lineX = 0, lineY = 0;
         if(diff < 0 || diff > 1) return 0;
         
-        if(formate(arr[1][1], arr[0][0], arr[2][2]) || formate(arr[2][0], arr[0][2], arr[1][1])) {
+        if(formateLine(arr[1][1], arr[0][0], arr[2][2]) || formateLine(arr[2][0], arr[0][2], arr[1][1])) {
             if(arr[1][1] == 'X') lineX++; if(arr[1][1] == 'O') lineY++;
         }
         
         for(int i = 0 ; i < 3 ; i++) {
-            if(formate(arr[i][0], arr[i][1], arr[i][2])) {
+            if(formateLine(arr[i][0], arr[i][1], arr[i][2])) {
                 if(arr[i][0] == 'X') lineX++; if(arr[i][0] == 'O') lineY++;
             }
-            if(formate(arr[0][i], arr[1][i], arr[2][i])) {
+            if(formateLine(arr[0][i], arr[1][i], arr[2][i])) {
                 if(arr[0][i] == 'X') lineX++; if(arr[0][i] == 'O') lineY++;
             }
         }
         if((lineX == lineY && lineY > 0) || (lineY > 0 && diff == 0) || (lineX > 0 && diff == 1)) return 0;
         return 1;
     }
-    public boolean formate(char a, char b, char c) {
+    public boolean formateLine(char a, char b, char c) {
         return a == b && b == c;
     }
 }
