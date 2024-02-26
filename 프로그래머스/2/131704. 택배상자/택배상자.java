2b7147745d4ff6733db474b15933d19880a89b70
박재우 <71738723+some1mine@users.noreply.github.com>
2024-02-26 23:35:37 +0900

    [level 2] Title: 택배상자, Time: 159.39 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -4,8 +4,8 @@ class Solution {
     public int solution(int[] order) {
         int answer = 0, idx = 0;
         Stack<Integer> sub = new Stack<>();
-        for(int i = 0 ; i < order.length ; i++) {
-            sub.push(i + 1);
+        for(int i = 1 ; i <= order.length ; i++) {
+            sub.push(i);
             while(!sub.isEmpty() && sub.peek() == order[idx]) {
                 sub.pop(); answer++; idx++;
             }
