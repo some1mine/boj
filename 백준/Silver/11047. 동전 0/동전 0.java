95a6c36fdb1480c8f07f7167898bb3bba5b34a17
박재우 <71738723+some1mine@users.noreply.github.com>
2024-03-01 01:23:19 +0900

    [Silver IV] Title: 동전 0, Time: 208 ms, Memory: 17888 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,20 +1,19 @@
 import java.util.*;
+import java.util.stream.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] nk = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] coins = new int[nk[0]];
-        for(int i = 0 ; i < nk[0] ; i++) coins[i] = Integer.parseInt(scanner.nextLine());
-        int[] dp = new int[nk[1] + 1]; Arrays.fill(dp, 111_111_111); dp[0] = 0; 
-        for(int coin : coins) {
-            if(coin <= nk[1]) dp[coin] = 1; 
-        }
-        for(int i = 0 ; i <= nk[1] ; i++) {
-            for(int coin : coins) {
-                if(i >= coin) dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+        Integer[] arr = new Integer[nk[0]];
+        int answer = 0;
+        for(int i = 0 ; i < nk[0] ; i++) arr[i] = Integer.parseInt(scanner.nextLine());
+        Arrays.sort(arr, Collections.reverseOrder());
+        for(int i : arr) {
+            if(nk[1] >= i) {
+                answer += nk[1] / i; nk[1] %= i;
             }
         }
-        System.out.print(dp[nk[1]]);
+        System.out.print(answer);
     }
 }
