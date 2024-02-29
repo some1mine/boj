9b239256ee45850fc1e00a7472455aa522517825
박재우 <71738723+some1mine@users.noreply.github.com>
2024-03-01 00:38:11 +0900

    [Bronze II] Title: 블랙잭, Time: 216 ms, Memory: 17564 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,23 +1,18 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt(), m = scanner.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0 ; i < n ; i += 1) arr[i] = scanner.nextInt();
-        int answer = 0;
-        loop1:
-        for(int i = 0 ; i < n ; i += 1) {
-            for(int j = i + 1 ; j < n ; j += 1) {
-                for(int k = j + 1; k < n ; k += 1) {
-                    int tmp = arr[i] + arr[j] + arr[k];
-                    if(tmp > m) continue;
-                    if(m - tmp < m - answer) answer = tmp;
-                    if(answer == m) break loop1;
+        Scanner scanner = new Scanner(System.in); int answer = -1;
+        int[] nm = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] arr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for(int i = 0 ; i < arr.length ; i++) {
+            for(int j = i + 1 ; j < arr.length ; j++) {
+                for(int k = j + 1 ; k < arr.length ; k++) {
+                    int tmp = Math.max(answer, arr[i] + arr[j] + arr[k]);
+                    answer = tmp <= nm[1] ? Math.max(answer, tmp) : answer;
                 }
             }
         }
-        System.out.println(answer);
+        System.out.print(answer);
     }
 }
