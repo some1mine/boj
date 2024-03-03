e2aa5899d44b765b7c1df76b34bd16e0465af99e
박재우 <71738723+some1mine@users.noreply.github.com>
2024-03-03 23:24:28 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 6.91 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -3,16 +3,16 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
-        List<Integer> divA = getDiv(arrayA, arrayB), divB = getDiv(arrayB, arrayA);
+        Set<Integer> divA = getDiv(arrayA, arrayB), divB = getDiv(arrayB, arrayA);
         return Math.max(divA.stream().mapToInt(i -> i).max().orElse(0), divB.stream().mapToInt(i -> i).max().orElse(0));
     }
-    public List<Integer> getDiv(int[] arr1, int[] arr2) {
-        List<Integer> dividors = getDiv(Arrays.stream(arr1).min().getAsInt());
+    public Set<Integer> getDiv(int[] arr1, int[] arr2) {
+        Set<Integer> dividors = getDiv(Arrays.stream(arr1).min().getAsInt());
         return dividors.stream().filter(d -> 
             Arrays.stream(arr1).noneMatch(a -> a % d > 0) && Arrays.stream(arr2).noneMatch(a -> a % d == 0)
-        ).collect(Collectors.toList());
+        ).collect(Collectors.toSet());
     }
-    public List<Integer> getDiv(int n) {
-        return IntStream.rangeClosed(1, n).filter(i -> n % i == 0).boxed().collect(Collectors.toList());
+    public Set<Integer> getDiv(int n) {
+        return IntStream.rangeClosed(1, n).filter(i -> n % i == 0).boxed().collect(Collectors.toSet());
     }
 }
