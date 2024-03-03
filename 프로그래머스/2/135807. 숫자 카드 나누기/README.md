83cc2ae1f70b3822bc3d7d96342beceb0548f726
박재우 <71738723+some1mine@users.noreply.github.com>
2024-03-03 23:23:09 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 6.63 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/README.md
+++ 프로그래머스/2/135807. 숫자 카드 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자 카드 나누기 - 135807 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135807?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135807) 
 
 ### 성능 요약
 
-메모리: 82.6 MB, 시간: 8.79 ms
+메모리: 68.9 MB, 시간: 6.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 21일 23:59:52
+2024년 03월 03일 23:23:06
 
 ### 문제 설명
 
--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -4,8 +4,7 @@ import java.util.stream.*;
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
         List<Integer> divA = getDiv(arrayA, arrayB), divB = getDiv(arrayB, arrayA);
-        return Math.max(divA.stream().filter(a -> !divB.contains(a)).mapToInt(i -> i).max().orElse(0),
-            divB.stream().filter(b -> !divA.contains(b)).mapToInt(i -> i).max().orElse(0));
+        return Math.max(divA.stream().mapToInt(i -> i).max().orElse(0), divB.stream().mapToInt(i -> i).max().orElse(0));
     }
     public List<Integer> getDiv(int[] arr1, int[] arr2) {
         List<Integer> dividors = getDiv(Arrays.stream(arr1).min().getAsInt());
