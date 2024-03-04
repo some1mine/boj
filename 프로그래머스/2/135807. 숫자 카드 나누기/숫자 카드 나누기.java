2747590a3de726621722ef594ea5c51678b15e03
박재우 <71738723+some1mine@users.noreply.github.com>
2024-03-04 23:43:00 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 4.46 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -3,14 +3,14 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
-        Set<Integer> divA = getDivs(arrayA, arrayB), divB = getDivs(arrayB, arrayA);
-        return Math.max(divA.stream().mapToInt(i -> i).max().orElse(0), divB.stream().mapToInt(i -> i).max().orElse(0));
+        return Math.max(getMaxDiv(arrayA, arrayB), getMaxDiv(arrayB, arrayA));
     }
-    public Set<Integer> getDivs(int[] arr1, int[] arr2) {
-        Set<Integer> divs = getDivs(Arrays.stream(arr1).min().getAsInt());
-        return divs.stream().filter(i -> Arrays.stream(arr1).noneMatch(n -> n % i > 0)
-                                        && Arrays.stream(arr2).noneMatch(n -> n % i == 0))
-            .collect(Collectors.toSet());
+    public int getMaxDiv(int[] arr1, int[] arr2) {
+        Set<Integer> divs = getDivs(Arrays.stream(arr1).min().getAsInt()); // 가장 작은 수의 모든 약수 구하기
+        return divs.stream().filter(i
+                    -> Arrays.stream(arr1).noneMatch(n -> n % i > 0)   // arr1에서 나누어 떨어지지 않는 수 filter
+                    && Arrays.stream(arr2).noneMatch(n -> n % i == 0)) // arr2에서 나누어 떨어지는 수 filter
+            .mapToInt(i -> i).max().orElse(0);
     }
     public Set<Integer> getDivs(int num) {
         return IntStream.rangeClosed(1, num).filter(i -> num % i == 0).boxed().collect(Collectors.toSet());
