30c7f067fa5361672b5cbc7588bac1288d0c18f3
박재우 <71738723+some1mine@users.noreply.github.com>
2024-03-04 23:31:13 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 4.83 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -3,18 +3,16 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
-        Set<Integer> divA = getDiv(arrayA, arrayB), divB = getDiv(arrayB, arrayA);
-        return Math.max(divA.stream().mapToInt(i -> i).max().orElse(0), 
-                        divB.stream().mapToInt(i -> i).max().orElse(0));
+        Set<Integer> divA = getDivs(arrayA, arrayB), divB = getDivs(arrayB, arrayA);
+        return Math.max(divA.stream().mapToInt(i -> i).max().orElse(0), divB.stream().mapToInt(i -> i).max().orElse(0));
     }
-    public Set<Integer> getDiv(int[] arr1, int[] arr2) {
-        Set<Integer> dividors = getDiv(Arrays.stream(arr1).min().getAsInt()); // 가장 작은 수의 모든 약수 구하기
-        return dividors.stream().filter(d -> 
-            Arrays.stream(arr1).noneMatch(a -> a % d > 0) // arr1에서 나누어 떨어지지 않는 수 filter
-            && Arrays.stream(arr2).noneMatch(a -> a % d == 0) // arr2에서 나누어 떨어지는 수 filter
-        ).collect(Collectors.toSet());
+    public Set<Integer> getDivs(int[] arr1, int[] arr2) {
+        Set<Integer> divs = getDivs(Arrays.stream(arr1).min().getAsInt());
+        return divs.stream().filter(i -> Arrays.stream(arr1).noneMatch(n -> n % i > 0)
+                                        && Arrays.stream(arr2).noneMatch(n -> n % i == 0))
+            .collect(Collectors.toSet());
     }
-    public Set<Integer> getDiv(int n) {
-        return IntStream.rangeClosed(1, n).filter(i -> n % i == 0).boxed().collect(Collectors.toSet());
+    public Set<Integer> getDivs(int num) {
+        return IntStream.rangeClosed(1, num).filter(i -> num % i == 0).boxed().collect(Collectors.toSet());
     }
 }
