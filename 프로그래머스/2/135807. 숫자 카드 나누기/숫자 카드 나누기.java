ab06719e2bf94819820a1997ebd80a9f21a193bd
박재우 <71738723+some1mine@users.noreply.github.com>
2024-03-04 23:50:49 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 4.25 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -6,13 +6,11 @@ class Solution {
         return Math.max(getMaxDiv(arrayA, arrayB), getMaxDiv(arrayB, arrayA));
     }
     public int getMaxDiv(int[] arr1, int[] arr2) {
-        Set<Integer> divs = getDivs(Arrays.stream(arr1).min().getAsInt()); // 가장 작은 수의 모든 약수 구하기
-        return divs.stream().filter(i
-                    -> Arrays.stream(arr1).noneMatch(n -> n % i > 0)   // arr1에서 나누어 떨어지지 않는 수 filter
+        int min = Arrays.stream(arr1).min().getAsInt();
+        return IntStream.rangeClosed(1, min).filter(i
+                    -> min % i == 0
+                    && Arrays.stream(arr1).noneMatch(n -> n % i > 0)   // arr1에서 나누어 떨어지지 않는 수 filter
                     && Arrays.stream(arr2).noneMatch(n -> n % i == 0)) // arr2에서 나누어 떨어지는 수 filter
-            .mapToInt(i -> i).max().orElse(0);
-    }
-    public Set<Integer> getDivs(int num) {
-        return IntStream.rangeClosed(1, num).filter(i -> num % i == 0).boxed().collect(Collectors.toSet());
+            .max().orElse(0);
     }
 }
