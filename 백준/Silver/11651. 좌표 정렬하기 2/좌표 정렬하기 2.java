f41bb8ece69405c2706054d2c6732bf044ccd36d
박재우 <71738723+some1mine@users.noreply.github.com>
2024-03-06 23:49:56 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2520 ms, Memory: 242004 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,36 +1,15 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int len = Integer.parseInt(br.readLine());
-        int[][] arr = new int[len][2];
-        String[] tmp = new String[2];
-        
-        for(int i = 0 ; i < len ; i++) {
-            tmp = br.readLine().split(" ");
-            arr[i][0] = Integer.parseInt(tmp[0]);
-            arr[i][1] = Integer.parseInt(tmp[1]);
-        }
-        
-        int[] numForCng;
-        
-        Arrays.sort(arr, (a, b) -> {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int[][] dots = new int[n][2];
+        for(int i = 0 ; i < n ; i++) dots[i] = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(dots, (a, b) -> {
             if(a[1] == b[1]) return a[0] - b[0];
-            else return a[1] - b[1];
+            return a[1] - b[1];
         });
-        for(int[] i : arr) {
-            System.out.println(i[0] + " " + i[1]);
-        }
-    }
-    
-    public static boolean haveToChange(int[] a, int[] b) {
-        boolean ret = false;
-        if(a[1] > b[1]) ret = true;
-        else if(a[1] == b[1] && a[0]  > b[0]) ret = true;
-        
-        return ret;
+        for(int i = 0 ; i < n ; i++) System.out.println(dots[i][0] + " " + dots[i][1]);
     }
 }
