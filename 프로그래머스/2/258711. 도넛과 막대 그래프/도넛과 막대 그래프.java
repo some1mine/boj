1c4b606d6b749c2b5f8d7e35f6ff17b52936947c
박재우 <71738723+some1mine@users.noreply.github.com>
2024-03-10 05:42:05 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 78.10 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -4,10 +4,10 @@ class Solution {
         for(int[] edge : edges) {
             conn[edge[0]][1]++; conn[edge[1]][0]++; max = Math.max(max, Math.max(edge[0], edge[1]));
         }
-        for(int i = 0 ; i < max + 1 ; i++) {
-            if(conn[i][0] == 0 && conn[i][1] > 1) answer[0] = i;
-            if(conn[i][0] > 0 && conn[i][1] == 0) answer[2]++;
-            if(conn[i][0] > 1 && conn[i][1] > 1) answer[3]++;
+        for(int i = 0 ; i <= max ; i++) {
+            if(conn[i][0] == 0 && conn[i][1] >= 2) answer[0] = i;
+            if(conn[i][0] >= 1 && conn[i][1] == 0) answer[2]++;
+            if(conn[i][0] >= 2 && conn[i][1] >= 2) answer[3]++;
         }
         answer[1] = conn[answer[0]][1] - answer[2] - answer[3];
         return answer;
