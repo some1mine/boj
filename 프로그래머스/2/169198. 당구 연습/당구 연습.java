bb3ba38318be8e15f4b9b65d7a87b4b9be1cc801
박재우 <71738723+some1mine@users.noreply.github.com>
2024-03-10 04:09:56 +0900

    [level 2] Title: 당구 연습, Time: 0.24 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/169198. 당구 연습/당구 연습.java
+++ 프로그래머스/2/169198. 당구 연습/당구 연습.java
@@ -4,13 +4,13 @@ class Solution {
         for(int i = 0 ; i < balls.length ; i++) result[i] = getDistance(m, n, x, y, balls[i]);
         return result;
     }
-    public int getDistance(int m, int n, int x, int y, int[] ball) { // 막혀서 못가는 경우 큰 값으로 거름
-        int d1 = (x - ball[0]) * (x - ball[0]), // 공 사이의 x거리
-            d3 = (y - ball[1]) * (y - ball[1]), // 공 사이의 y거리
-            d2 = (2 * n - y - ball[1]) * (2 * n - y - ball[1]), // 벽과 공과의 y거리 합
-            d4 = (2 * m - x - ball[0]) * (2 * m - x - ball[0]), // 벽과 공과의 x거리 합
-            d5 = (y + ball[1]) * (y + ball[1]), // 공 사이의 y 거리 합
-            d6 = (x + ball[0]) * (x + ball[0]); // 공 사이의 x 거리 합
+    public int getDistance(int m, int n, int x, int y, int[] ball) {    // 막혀서 못가는 경우 큰 값으로 거름
+        int d1 = (x - ball[0]) * (x - ball[0]),                         // 공 사이의 x거리
+            d3 = (y - ball[1]) * (y - ball[1]),                         // 공 사이의 y거리
+            d2 = (2 * n - y - ball[1]) * (2 * n - y - ball[1]),         // 벽과 공과의 y거리 합
+            d4 = (2 * m - x - ball[0]) * (2 * m - x - ball[0]),         // 벽과 공과의 x거리 합
+            d5 = (y + ball[1]) * (y + ball[1]),                         // 공 사이의 y 거리 합
+            d6 = (x + ball[0]) * (x + ball[0]);                         // 공 사이의 x 거리 합
                 
         return  Math.min(ball[1] > y && x == ball[0] ? Integer.MAX_VALUE : d1 + d2,
                 Math.min(ball[0] > x && y == ball[1] ? Integer.MAX_VALUE : d3 + d4,
