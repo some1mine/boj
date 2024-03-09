fc45b4b05de1d755537c4034edfa87299521529c
박재우 <71738723+some1mine@users.noreply.github.com>
2024-03-10 04:14:05 +0900

    [level 2] Title: 당구 연습, Time: 0.50 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/169198. 당구 연습/README.md
+++ 프로그래머스/2/169198. 당구 연습/README.md
@@ -1,10 +1,10 @@
 # [level 2] 당구 연습 - 169198 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/169198#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/169198) 
 
 ### 성능 요약
 
-메모리: 79.7 MB, 시간: 0.24 ms
+메모리: 77.4 MB, 시간: 0.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 10일 04:09:54
+2024년 03월 10일 04:14:04
 
 ### 문제 설명
 
--- 프로그래머스/2/169198. 당구 연습/당구 연습.java
+++ 프로그래머스/2/169198. 당구 연습/당구 연습.java
@@ -6,15 +6,15 @@ class Solution {
     }
     public int getDistance(int m, int n, int x, int y, int[] ball) {    // 막혀서 못가는 경우 큰 값으로 거름
         int d1 = (x - ball[0]) * (x - ball[0]),                         // 공 사이의 x거리
-            d3 = (y - ball[1]) * (y - ball[1]),                         // 공 사이의 y거리
-            d2 = (2 * n - y - ball[1]) * (2 * n - y - ball[1]),         // 벽과 공과의 y거리 합
+            d2 = (y - ball[1]) * (y - ball[1]),                         // 공 사이의 y거리
+            d3 = (2 * n - y - ball[1]) * (2 * n - y - ball[1]),         // 벽과 공과의 y거리 합
             d4 = (2 * m - x - ball[0]) * (2 * m - x - ball[0]),         // 벽과 공과의 x거리 합
             d5 = (y + ball[1]) * (y + ball[1]),                         // 공 사이의 y 거리 합
             d6 = (x + ball[0]) * (x + ball[0]);                         // 공 사이의 x 거리 합
                 
-        return  Math.min(ball[1] > y && x == ball[0] ? Integer.MAX_VALUE : d1 + d2,
-                Math.min(ball[0] > x && y == ball[1] ? Integer.MAX_VALUE : d3 + d4,
+        return  Math.min(ball[1] > y && x == ball[0] ? Integer.MAX_VALUE : d1 + d3,
+                Math.min(ball[0] > x && y == ball[1] ? Integer.MAX_VALUE : d2 + d4,
                 Math.min(ball[1] < y && x == ball[0] ? Integer.MAX_VALUE : d1 + d5,
-                         ball[0] < x && y == ball[1] ? Integer.MAX_VALUE : d3 + d6)));
+                         ball[0] < x && y == ball[1] ? Integer.MAX_VALUE : d2 + d6)));
     }
 }
