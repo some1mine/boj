01fb898d570ebc205fdc62d1b3ab8c0f5c828803
박재우 <71738723+some1mine@users.noreply.github.com>
2024-03-10 13:05:45 +0900

    [Gold IV] Title: 공유기 설치, Time: 316 ms, Memory: 29392 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -4,38 +4,25 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(reader.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(reader.readLine());
         int n = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
         int[] arr = new int[n];
-        for(int i = 0; i < n ; i++) arr[i] = Integer.parseInt(reader.readLine());
+        for(int i = 0 ; i < n ; i++) arr[i] = Integer.parseInt(reader.readLine());
+        int left = 0, right = arr[n - 1];
         Arrays.sort(arr);
-        
-        int answer = -1;
-        /* 최단 거리와 최장 거리 */
-        int start = 1, end = arr[n - 1] - arr[0]; 
-        
-        /* 이분 탐색 */
-        while(start <= end) {
-            /* 그 중간 */
-            int mid = (start + end) / 2; 
-            /* 중간을 잡고 그 이상 거리들을 셈 */
-            int cnt = 1; int cur = arr[0];
-            /* 집 순회 */
-            for(int num : arr) {
-                /* 거리가 이상이라면 */
-                if (num - cur >= mid) { 
-                    /* cnt증가, 다음 집으로 갱신 */
-                    cnt++; cur = num; 
-                }
-            }
-            /* 타켓 거리 이상 떨어진 거리의 수가 설치할 공유기 수 이상이라면 */
-            if (cnt >= c) { 
-                /* possible, 거리 더 벌림 */
-                answer = mid; start = mid + 1; 
-                /* 미만인 경우 impossible, 거리 더 좁힘 */
-            } else end = mid - 1; 
+        while(left <= right) {
+            if(getCount(left, right, arr) < c) right = (left + right) / 2 - 1;
+            else left = (left + right) / 2 + 1;
         }
-        System.out.println(answer);
-        reader.close();
+        System.out.print(right);
+    }
+    public static int getCount(int left, int right, int[] arr) {
+        int answer = 1, cur = arr[0], mid = (left + right) / 2; 
+        for(int i : arr) {
+            if(i - cur >= mid) {
+                answer++; cur = i;
+            }
+        }
+        return answer;
     }
 }
