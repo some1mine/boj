936d4c75c600ab6c6df1d20eb76897738c143883
박재우 <71738723+some1mine@users.noreply.github.com>
2024-03-10 13:20:48 +0900

    [Gold I] Title: K번째 수, Time: 164 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -5,14 +5,17 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
-        long k = Long.parseLong(reader.readLine()), start = 1, end = k;
+        long k = Long.parseLong(reader.readLine()), left = 1, right = k;
         
-        while (start < end) {
-            long mid = (start + end) / 2, cnt = 0;
-            for (int i = 1; i <= n; i++) cnt += Math.min(mid / i, n);
-            if(k > cnt) start = mid + 1;
-            else end = mid;
+        while (left < right) {
+            if(k > getCount(left, right, n)) left = (left + right) / 2 + 1;
+            else right = (left + right) / 2;
         }
-        System.out.print(start);
+        System.out.print(left);
+    }
+    public static long getCount(long left, long right, int n) {
+        long answer = 0;
+        for(int i = 1 ; i <= n ; i++) answer += Math.min((left + right) / 2 / i, n);
+        return answer;
     }
 }
