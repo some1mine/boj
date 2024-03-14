ef909427453b8838e055cd5e4785c407e623a76c
박재우 <71738723+some1mine@users.noreply.github.com>
2024-03-14 20:16:01 +0900

    [Silver I] Title: 정수 삼각형, Time: 572 ms, Memory: 36868 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,19 +1,18 @@
 import java.util.*;
- 
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
-        int[][] triangle = new int[n][];
-        int[][] dp = new int[n][n];
-        for(int i = 0 ; i < n ; i++) triangle[i] = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        dp[0][0] = triangle[0][0];
+        int[][] arr = new int[n][], dp = new int[n][n];
+        for(int i = 0 ; i < n ; i++) arr[i] = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        dp[0][0] = arr[0][0];
         for(int i = 1 ; i < n ; i++) {
             for(int j = 0 ; j <= i ; j++) {
-                dp[i][j] = Math.max(dp[i][j], dp[i - 1][j] + triangle[i][j]);
-                if(j > 0) dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + triangle[i][j]);
+                dp[i][j] = Math.max(dp[i][j], dp[i - 1][j] + arr[i][j]);
+                if(j > 0) dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + arr[i][j]);
             }
         }
-        System.out.print(Arrays.stream(dp).flatMapToInt(Arrays::stream).max().getAsInt());
+        System.out.print(Arrays.stream(dp[n - 1]).max().getAsInt());
     }
 }
