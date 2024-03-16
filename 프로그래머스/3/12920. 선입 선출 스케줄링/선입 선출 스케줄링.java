d0be55e63db04dd8f237c57f4f51659212d9e906
박재우 <71738723+some1mine@users.noreply.github.com>
2024-03-17 06:48:08 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 1.63 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
+++ 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int n, int[] cores) {
-        int answer = 0;
         if(n <= cores.length) return n;
 
         int min = 1, max = (n * 10000) / cores.length, time = 0, cnt = cores.length;
@@ -14,11 +13,8 @@ class Solution {
         int delta = cnt - n;
         for(int i = cores.length - 1 ; i > -1 ; i--){
             if(time % cores[i] == 0) delta--;
-            if(delta < 0) {
-                answer = i + 1; break;
-            }
+            if(delta < 0) return i + 1;
         }
-
-        return answer;
+        return -1;
     }
 }
