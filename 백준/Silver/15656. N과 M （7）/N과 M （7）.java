12443c6c68f9f87f88ec284b377d98f07254fd30
박재우 <71738723+some1mine@users.noreply.github.com>
2024-04-13 00:02:34 +0900

    [Silver III] Title: N과 M (7), Time: 772 ms, Memory: 165396 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.java
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -2,20 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static StringBuffer sb = new StringBuffer();
+    public static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int[] nm = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[] arr = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
         permutation(0, nm[1], arr, new int[nm[1]]);
-        System.out.println(sb);
+        writer.flush();
+        writer.close();
     }
-    public static void permutation(int idx, int m, int[] arr, int[] results) {
+    public static void permutation(int idx, int m, int[] arr, int[] results) throws IOException {
         if (idx == m) {
-            for (int i : results) sb.append(i).append(" ");
-            sb.append("\n");
+            for (int i : results) writer.append(i + " ");
+            writer.append("\n");
             return;
         }
+
         for (int n : arr) {
             results[idx] = n;
             permutation(idx + 1, m, arr, results);
