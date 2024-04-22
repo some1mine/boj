984c8f9fef60c15a6dc28b872617c516c754d17b
박재우 <71738723+some1mine@users.noreply.github.com>
2024-04-22 22:43:17 +0900

    [level 2] Title: 행렬의 곱셈, Time: 43.92 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -5,8 +5,7 @@ class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         final int[][] a1 = arr1, a2 = arr2;
         return IntStream.range(0, arr1.length)
-            .mapToObj(n1 -> 
-                     IntStream.range(0, arr2[0].length)
+            .mapToObj(n1 -> IntStream.range(0, arr2[0].length)
                      .map(n2 -> IntStream.range(0, arr2.length)
                                .map(n3 -> a1[n1][n3] * a2[n3][n2]).sum()).toArray())
             .toArray(int[][]::new);
