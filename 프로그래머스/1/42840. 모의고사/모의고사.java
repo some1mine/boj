c6643478e6e7d2b3f6d4e91928f160b717d79ffc
박재우 <71738723+some1mine@users.noreply.github.com>
2024-04-22 21:46:04 +0900

    [level 1] Title: 모의고사, Time: 11.59 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,8 +3,7 @@ import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        final int[] arr1 = {1, 2, 3, 4, 5}, arr2 = {2, 1, 2, 3, 2, 4, 2, 5}, arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        final int[] answerArr = answers;
+        final int[] arr1 = {1, 2, 3, 4, 5}, arr2 = {2, 1, 2, 3, 2, 4, 2, 5}, arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}, answerArr = answers;
         final boolean[][] scores = IntStream.range(0, answers.length)
             .mapToObj(n -> 
                       new boolean[]{arr1[n % arr1.length] == answerArr[n], 
