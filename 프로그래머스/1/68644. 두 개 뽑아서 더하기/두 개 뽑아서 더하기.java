6ccea438c91178544c0e2006d42e7d178a86b189
박재우 <71738723+some1mine@users.noreply.github.com>
2024-04-22 21:22:52 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 8.57 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        return IntStream.range(0, numbers.length)
+            .mapToObj(n1 -> IntStream.range(n1 + 1, numbers.length).map(n2 -> numbers[n1] + numbers[n2]).toArray())
+            .flatMapToInt(Arrays::stream).sorted().boxed().distinct().mapToInt(i -> i).toArray();
+    }
+}
