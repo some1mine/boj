a631c2518988e32c82d934773bd0a5455fa00c0a
박재우 <71738723+some1mine@users.noreply.github.com>
2024-06-09 11:40:26 +0900

    [level 4] Title: 도둑질, Time: 23.05 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,17 +1,13 @@
 class Solution {
     public int solution(int[] money) {      
         if(money.length == 3) return Math.max(money[2], Math.max(money[0], money[1]));
-        
-        int[] dp1 = new int[money.length], dp2 = new int[money.length]; 
-        dp1[0] = money[0]; dp1[1] = money[1]; dp2[1] = money[1];
+        int[][] dp = new int[2][money.length]; dp[0][0] = money[0]; dp[0][1] = money[1]; dp[1][1] = money[1];
         for(int i = 2 ; i < money.length ; i++) {
-            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + money[i]);
-            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + money[i]);
-            if(i > 2) {
-                dp1[i] = Math.max(dp1[i], dp1[i - 3] + money[i]);
-                dp2[i] = Math.max(dp2[i], dp2[i - 3] + money[i]);
+            for(int j = 0 ; j < 2 ; j++) {
+                dp[j][i] = Math.max(dp[j][i - 1], dp[j][i - 2] + money[i]);
+                if(i > 2) dp[j][i] = Math.max(dp[j][i], dp[j][i - 3] + money[i]);
             }
         }
-        return Math.max(dp1[money.length - 2], dp2[money.length - 1]);
+        return Math.max(dp[0][money.length - 2], dp[1][money.length - 1]);
     }
 }
