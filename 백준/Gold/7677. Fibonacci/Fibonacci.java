3be1153e20a8a928f2e5f8947ebd0e481091f424
박재우 <71738723+some1mine@users.noreply.github.com>
2024-07-05 20:42:12 +0900

    [Gold II] Title: Fibonacci, Time: 236 ms, Memory: 19332 KB -BaekjoonHub

---
+++ 백준/Gold/7677. Fibonacci/Fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static Map<Long, Long> map = new HashMap<>();
+    public static int mod = 10000; 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        map.put(0L, 0L); map.put(1L, 1L); map.put(2L, 1L); long n;
+        while((n = Long.parseLong(scanner.nextLine())) != -1) System.out.println(get(n));
+    }
+    public static long get(long n) {
+        if(map.containsKey(n)) return map.get(n);
+        if(n % 2 == 0){
+            long n1 = n / 2, n2 = (n / 2) - 1,
+                num = (get(n1) % mod * (get(n1) % mod + (2 * get(n2) % mod) % mod) % mod) % mod;
+            map.put(n, num);
+            return num;
+        }
+        long n1 = (n - 1) / 2, n2 = (n + 1) / 2,
+            num = ((get(n1) * get(n1)) % mod + (get(n2) * get(n2)) % mod ) % mod;
+        map.put(n, num);
+        return num;
+    }
+}
