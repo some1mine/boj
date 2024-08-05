7422aafa52b56ff3f3fee58e7e69e1b2628877bd
박재우 <71738723+some1mine@users.noreply.github.com>
2024-08-05 11:25:42 +0900

    [Silver V] Title: Sport Climbing Combined, Time: 100 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/23246. Sport Climbing Combined/Sport Climbing Combined.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+def comp(a):
+	return ((a[1] * a[2] * a[3]), (a[1] + a[2] + a[3]), a[0])
+
+n = int(input())
+arr = sorted([list(map(int, input().split())) for _ in range(n)], key=comp)
+for i in range(3): print(arr[i][0])
