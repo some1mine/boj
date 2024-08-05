d644fea0c4426af1579747e9f666009d049fa2a9
박재우 <71738723+some1mine@users.noreply.github.com>
2024-08-05 16:16:56 +0900

    [Silver II] Title: 부분수열의 합, Time: 472 ms, Memory: 126588 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
+
+cnt = 0; choose = []
+
+def combination(idx, level):
+	global cnt
+	if choose and sum(choose) == s: cnt += 1
+	for i in range(idx, n):
+		choose.append(arr[i])
+		combination(i + 1, level + 1)
+		choose.remove(arr[i])
+
+combination(0, 0)
+print(cnt)
