aa9bc4bba1e961320d302e7a84d1a6d5f2d0793a
박재우 <71738723+some1mine@users.noreply.github.com>
2024-08-09 14:32:01 +0900

    [Silver II] Title: 이동하기, Time: 196 ms, Memory: 120128 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -7,6 +7,6 @@ for i in range(1, n): arr[i][0] += arr[i - 1][0]
 for i in range(1, m): arr[0][i] += arr[0][i - 1]
 for i in range(1, n):
 	for j in range(1, m):
-		arr[i][j] = max(arr[i][j], max(arr[i][j - 1] + arr[i][j], max(arr[i - 1][j] + arr[i][j], arr[i - 1][j - 1] + arr[i][j])))
+		arr[i][j] = max(arr[i][j - 1] + arr[i][j], max(arr[i - 1][j] + arr[i][j], arr[i - 1][j - 1] + arr[i][j]))
 
 print(max(map(max, arr)))
