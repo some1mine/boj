16af50e9b26715fe0bc09e74db6b0d8c4a8809a4
박재우 <71738723+some1mine@users.noreply.github.com>
2024-08-09 14:28:23 +0900

    [Silver II] Title: 이동하기, Time: 272 ms, Memory: 120668 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -4,11 +4,10 @@ input = sys.stdin.readline
 
 n, m = map(int, input().rstrip().split())
 arr = [list(map(int, input().rstrip().split())) for _ in range(n)]
-result = copy.deepcopy(arr)
-for i in range(1, n): result[i][0] += result[i - 1][0]
-for i in range(1, m): result[0][i] += result[0][i - 1]
+for i in range(1, n): arr[i][0] += arr[i - 1][0]
+for i in range(1, m): arr[0][i] += arr[0][i - 1]
 for i in range(1, n):
 	for j in range(1, m):
-		result[i][j] = max(result[i][j], max(result[i][j - 1] + arr[i][j], max(result[i - 1][j] + arr[i][j], result[i - 1][j - 1] + arr[i][j])))
+		arr[i][j] = max(arr[i][j], max(arr[i][j - 1] + arr[i][j], max(arr[i - 1][j] + arr[i][j], arr[i - 1][j - 1] + arr[i][j])))
 
-print(max(map(max, result)))
+print(max(map(max, arr)))
