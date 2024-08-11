d3d369e3ecad024fb730511bf3967824ac387057
박재우 <71738723+some1mine@users.noreply.github.com>
2024-08-11 21:23:17 +0900

    [Gold V] Title: 평범한 배낭, Time: 372 ms, Memory: 189588 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -10,4 +10,4 @@ for i in range(1, n + 1):
 		if arr[i - 1][0] <= j: dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - arr[i - 1][0]] + arr[i - 1][1])
 		else: dp[i][j] = dp[i - 1][j]
 
-print(max(dp[n]))
+print(dp[n][k])
