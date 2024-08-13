df3ecad97096b3a2c9598aa92f3b44b622f584f9
박재우 <71738723+some1mine@users.noreply.github.com>
2024-08-13 17:14:09 +0900

    [Silver II] Title: 연속합, Time: 132 ms, Memory: 120816 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -2,10 +2,7 @@ import sys
 input = sys.stdin.readline
 n = int(input().rstrip())
 arr = list(map(int, input().rstrip().split()))
-dp = [[0, 0] for _ in range(n)]; dp[0][1] = arr[0]
 for i in range(1, n):
-	dp[i][1] = max(dp[i - 1][1] + arr[i], 0)
-	dp[i][0] = arr[i]
+	if arr[i - 1] > 0 and arr[i] + arr[i - 1] > 0: arr[i] += arr[i - 1]
 
-res = max(map(max, dp))
-print(res if res > 0 else max(arr))
+print(max(arr))
