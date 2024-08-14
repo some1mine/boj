ed1da852674bfcec2557b467555cd9f49afc8622
박재우 <71738723+some1mine@users.noreply.github.com>
2024-08-14 12:09:21 +0900

    [Gold IV] Title: 알파벳, Time: 3612 ms, Memory: 133816 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,10 +1,10 @@
 r, c = map(int, input().split())
-arr = [input() for _ in range(r)]; chars = [False for _ in range(26)]
+arr = [input() for _ in range(r)]; chars = [False for _ in range(ord('z') - ord('a') + 1)]
 cnt = 0; length = 0
 directions = [[0,-1],[0,1],[-1,0],[1,0]]
 def go(y, x):
 	global cnt, chars, length
-	if cnt > 25 or y < 0 or x < 0 or y >= r or x >= c or chars[ord(arr[y][x]) - ord('A')]: return
+	if cnt > ord('z') - ord('a') or y < 0 or x < 0 or y >= r or x >= c or chars[ord(arr[y][x]) - ord('A')]: return
 	chars[ord(arr[y][x]) - ord('A')] = True; length += 1
 	cnt = max(cnt, length)
 	for d in directions:
