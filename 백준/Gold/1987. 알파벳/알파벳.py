36a2682b938355a90911ec9e8b7acbc949fde42e
박재우 <71738723+some1mine@users.noreply.github.com>
2024-08-14 11:15:40 +0900

    [Gold IV] Title: 알파벳, Time: 6248 ms, Memory: 146448 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,16 +1,18 @@
-r, c = map(int, input().split())
+import sys
+input = sys.stdin.readline
+r, c = map(int, input().rstrip().split())
 arr = []
 for i in range(r): arr.append(input().rstrip())
 directions = [[0,-1],[0,1],[-1,0],[1,0]]
-s = set(arr[0][0]); cnt = 0
-def go(y, x, prev):
+s = arr[0][0]; cnt = 0
+def go(y, x):
 	global cnt, s
 	cnt = max(cnt, len(s))
 	for d in directions:
 		dy = y + d[0]; dx = x + d[1]
-		if (prev[0] == -d[0] and prev[1] == -d[1]) or dy < 0 or dx < 0 or dy >= r or dx >= c or arr[dy][dx] in s: continue
-		s.add(arr[dy][dx])
-		go(dy, dx, d)
-		s.remove(arr[dy][dx])
-go(0, 0, [1,1])
+		if dy < 0 or dx < 0 or dy >= r or dx >= c or arr[dy][dx] in s: continue
+		s += arr[dy][dx]
+		go(dy, dx)
+		s = s[:len(s) - 1]
+go(0, 0)
 print(cnt)
