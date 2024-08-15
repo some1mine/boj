af7b9ee859bd5c24ae407f22720dfe733b0b6793
박재우 <71738723+some1mine@users.noreply.github.com>
2024-08-15 16:11:48 +0900

    [Gold IV] Title: 스도쿠, Time: 616 ms, Memory: 115772 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,21 +1,5 @@
-def is_possible(y, x, n):
-	global matrix
-
-	for c in range(9):
-		if matrix[y][c] == n:
-			return False
-	for r in range(9):
-		if matrix[r][x] == n:
-			return False
-	for r in range(3):
-		for c in range(3):
-			if matrix[3 * (y // 3) + r][3 * (x // 3) + c] == n:
-				return False
-
-	return True
-
 def search(lev):
-	global matrix, pos
+	global row, col, square, matrix, pos
 
 	# base case
 	if lev == len(pos): # 스도쿠 완성
@@ -24,17 +8,25 @@ def search(lev):
 				print(matrix[i][j], end=' ')
 			print()
 		exit(0)
-		return
 
 	y, x = pos[lev]
 
 	# recursive case
 	for n in range(1, 10):
-		if is_possible(y, x, n):
+		if (not row[y][n]) and (not col[x][n]) and (not square[y // 3][x // 3][n]):
+			row[y][n] = col[x][n] = square[y // 3][x // 3][n] = True
 			matrix[y][x] = n
-			search(lev + 1)
-			matrix[y][x] = 0
 
+			search(lev + 1)
+
+			matrix[y][x] = 0
+			row[y][n] = col[x][n] = square[y // 3][x // 3][n] = False
+
+
+#initial settings
+row = [[False] * 10 for _ in range(9)]
+col = [[False] * 10 for _ in range(9)]
+square = [[[False] * 10 for _ in range(3)] for _ in range(3)]
 
 # input
 matrix = [list(map(int, input().split())) for _ in range(9)]
@@ -46,5 +38,7 @@ for i in range(9):
 		cur = matrix[i][j]
 		if cur == 0:
 			pos.append((i, j))
+		else:
+			row[i][cur] = col[j][cur] = square[i // 3][j // 3][cur] = True
 
 search(0)
