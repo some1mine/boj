a067bdf2113e74012cf07917b5c5fc5dc3659d2b
박재우 <71738723+some1mine@users.noreply.github.com>
2024-08-17 01:00:43 +0900

    [Gold IV] Title: 스도쿠, Time: 616 ms, Memory: 115712 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,44 +1,24 @@
-def search(lev):
-	global row, col, square, matrix, pos
-
-	# base case
-	if lev == len(pos): # 스도쿠 완성
-		for i in range(9):
-			for j in range(9):
-				print(matrix[i][j], end=' ')
-			print()
-		exit(0)
-
-	y, x = pos[lev]
-
-	# recursive case
-	for n in range(1, 10):
-		if (not row[y][n]) and (not col[x][n]) and (not square[y // 3][x // 3][n]):
-			row[y][n] = col[x][n] = square[y // 3][x // 3][n] = True
-			matrix[y][x] = n
-
-			search(lev + 1)
-
-			matrix[y][x] = 0
-			row[y][n] = col[x][n] = square[y // 3][x // 3][n] = False
-
-
-#initial settings
-row = [[False] * 10 for _ in range(9)]
-col = [[False] * 10 for _ in range(9)]
-square = [[[False] * 10 for _ in range(3)] for _ in range(3)]
-
-# input
-matrix = [list(map(int, input().split())) for _ in range(9)]
-
-# solve
-pos = []
+arr = [list(map(int, input().split())) for _ in range(9)]
+idxs = []
+row = [[False] * 10 for _ in range(9)]; col = [[False] * 10 for _ in range(9)]
+square  = [[[False] * 10 for _ in range(3)] for _ in range(3)]
 for i in range(9):
 	for j in range(9):
-		cur = matrix[i][j]
-		if cur == 0:
-			pos.append((i, j))
-		else:
-			row[i][cur] = col[j][cur] = square[i // 3][j // 3][cur] = True
-
-search(0)
+		if not arr[i][j]: idxs.append((i,j))
+		else: row[i][arr[i][j]] = col[j][arr[i][j]] = square[i // 3][j // 3][arr[i][j]] = True
+def solution(depth):
+	global arr, row, col, square
+	if depth == len(idxs):
+		for a in arr:
+			for i in a: print(i, end = ' ')
+			print()
+		exit(0)
+	y, x = idxs[depth]
+	for i in range(1, 10):
+		if not (row[y][i] or col[x][i] or square[y // 3][x // 3][i]):
+			row[y][i] = col[x][i] = square[y // 3][x // 3][i] = True
+			arr[y][x] = i
+			solution(depth + 1)
+			arr[y][x] = 0
+			row[y][i] = col[x][i] = square[y // 3][x // 3][i] = False
+solution(0)
