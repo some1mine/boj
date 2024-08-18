0c85cba336ea9e328cf79e4616a1f3828e4d9e76
박재우 <71738723+some1mine@users.noreply.github.com>
2024-08-19 06:41:43 +0900

    [Gold IV] Title: 알파벳, Time: 1024 ms, Memory: 18816 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+	public static int[] rc;
+	public static char[][] arr;
+	public static int cnt = 0, chars = 0, length = 0;
+	public static int[][] directions = {{0,1},{1,0},{-1,0},{0,-1}};
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		rc = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		arr = new char[rc[0]][rc[1]];
+		for(int i = 0 ; i < rc[0] ; i++) arr[i] = scanner.nextLine().toCharArray();
+		go(0, 0);
+		System.out.print(cnt);
+	}
+	public static void go(int y, int x) {
+		if(cnt > 'z' - 'a') return;
+		if(y < 0 || x < 0 || y >= rc[0] || x >= rc[1]) return;
+		if((chars & (1 << arr[y][x])) > 0) return;
+		length++; cnt = Math.max(cnt, length); chars |= (1 << arr[y][x]);
+		for(int[] d : directions) {
+			int dy = y + d[0], dx = x + d[1]; go(dy, dx);
+		}
+		if((chars & (1 << arr[y][x])) > 0) chars -= (1 << arr[y][x]);
+		length--;
+	}
+}
