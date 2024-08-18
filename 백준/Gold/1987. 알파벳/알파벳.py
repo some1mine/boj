eb5be803477a88dded5f9196473c9cf8f9651f0e
박재우 <71738723+some1mine@users.noreply.github.com>
2024-08-19 06:55:53 +0900

    [Gold IV] Title: 알파벳, Time: 3560 ms, Memory: 133916 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,15 +1,15 @@
-r, c = map(int, input().split())
-arr = [input() for _ in range(r)]; chars = [False for _ in range(ord('z') - ord('a') + 1)]
-cnt = 0; length = 0
-directions = [[0,-1],[0,1],[-1,0],[1,0]]
+r, c = map(int, input().split()); cnt = 0; chars = 0; length = 0
+arr = [input() for _ in range(r)]; directions = [[0,-1],[0,1],[-1,0],[1,0]]
 def go(y, x):
 	global cnt, chars, length
-	if cnt > ord('z') - ord('a') or y < 0 or x < 0 or y >= r or x >= c or chars[ord(arr[y][x]) - ord('A')]: return
-	chars[ord(arr[y][x]) - ord('A')] = True; length += 1
-	cnt = max(cnt, length)
+	if cnt > ord('z') - ord('a'): return # 모든 알파벳 돎
+	if y < 0 or x < 0 or y >= r or x >= c: return # 범위 벗어남
+	if chars & (1 << (ord(arr[y][x]) - ord('A'))): return # 이미 거친 알파벳일 때
+	chars |= (1 << ord(arr[y][x]) - ord('A'))
+	length += 1; cnt = max(cnt, length)
 	for d in directions:
 		dy = y + d[0]; dx = x + d[1]
 		go(dy, dx)
-	chars[ord(arr[y][x]) - ord('A')] = False; length -= 1
+	chars -= (1 << ord(arr[y][x]) - ord('A')); length -= 1
 go(0, 0)
 print(cnt)
