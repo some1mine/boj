5e4de80c139b9c607f35999a006a6aa65c14ea84
박재우 <71738723+some1mine@users.noreply.github.com>
2024-08-19 07:04:27 +0900

    [Gold IV] Title: 알파벳, Time: 3556 ms, Memory: 135076 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -2,14 +2,14 @@ r, c = map(int, input().split()); cnt = 0; chars = 0; length = 0
 arr = [input() for _ in range(r)]; directions = [[0,-1],[0,1],[-1,0],[1,0]]
 def go(y, x):
 	global cnt, chars, length
-	if cnt > ord('z') - ord('a'): return # 모든 알파벳 돎
 	if y < 0 or x < 0 or y >= r or x >= c: return # 범위 벗어남
-	if chars & (1 << (ord(arr[y][x]) - ord('A'))): return # 이미 거친 알파벳일 때
-	chars |= (1 << ord(arr[y][x]) - ord('A'))
-	length += 1; cnt = max(cnt, length)
+	if cnt > 25: return # 알파벳 전부 거쳐옴
+	toBinary = 1 << (ord(arr[y][x]) - ord('A'))
+	if chars & toBinary: return # 이미 거쳐온 알파벳일 때
+	chars |= toBinary; length += 1; cnt = max(cnt, length)
 	for d in directions:
 		dy = y + d[0]; dx = x + d[1]
 		go(dy, dx)
-	chars -= (1 << ord(arr[y][x]) - ord('A')); length -= 1
+	chars -= toBinary; length -= 1
 go(0, 0)
 print(cnt)
