b2e4f82b56cc2941278e1e61c8dcf677df2a89bf
박재우 <71738723+some1mine@users.noreply.github.com>
2025-01-02 23:25:13 +0900

    [Silver IV] Title: 수 찾기, Time: 1972 ms, Memory: 102728 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,33 +1,20 @@
-import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
-        int n = Integer.parseInt(reader.readLine());
-        StringTokenizer st = new StringTokenizer(reader.readLine());
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(arr);
-        int m = Integer.parseInt(reader.readLine());
-        st = new StringTokenizer(reader.readLine());
-        for(int i = 0 ; i < m ; i++) {
-            if(contains(arr, Integer.parseInt(st.nextToken()))) sb.append(1).append("\n");
-            else sb.append(0).append("\n");
-        }
-        System.out.println(sb.toString());
-        reader.close();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] arr1 = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+        int m = Integer.parseInt(scanner.nextLine());
+        int[] arr2 = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for(int i : arr2) binarySearch(arr1, i);
     }
-
-    private static boolean contains(int[] arr, int target) {
-        int start = 0, end = arr.length - 1;
-        while(start <= end) {
-            int half = (start + end) / 2;
-            if(arr[half] < target) start = half + 1;
-            if(arr[half] > target) end = half - 1;
-            if(arr[half] == target) return true;
+    public static void binarySearch(int[] arr, int target) {
+        int cur = -1, step = arr.length;
+        while(step > 0) {
+            while(cur + step < arr.length && arr[cur + step] <= target) cur += step;
+            step /= 2;
         }
-        return false;
+        System.out.println(cur < 0 || cur >= arr.length || arr[cur] != target ? 0 : 1);
     }
 }
