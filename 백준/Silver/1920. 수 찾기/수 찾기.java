4b5ffa3c4cf3c9bf2892ea7937b0855f1bb39510
박재우 <71738723+some1mine@users.noreply.github.com>
2025-01-02 23:41:12 +0900

    [Silver IV] Title: 수 찾기, Time: 404 ms, Memory: 58544 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,22 +1,18 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        scanner.nextLine();
-        int[] arr1 = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
-        scanner.nextLine();
-        int[] arr2 = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        for(int i : arr2) System.out.println(binarySearch(arr1, i));
-    }
-    public static int binarySearch(int[] arr, int target) {
-        int start = 0, end = arr.length - 1, mid = -1;
-        while(start <= end) {
-            mid = (start + end) / 2;
-            if(arr[mid] > target) end = mid - 1;
-            if(arr[mid] < target) start = mid + 1;
-            if(arr[mid] == target) return 1;
-        }
-        return 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        reader.readLine();
+        Set<String> set = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        while(st.hasMoreTokens()) set.add(st.nextToken());
+        reader.readLine();
+        st = new StringTokenizer(reader.readLine());
+        while(st.hasMoreTokens()) sb.append(set.contains(st.nextToken()) ? 1 : 0).append("\n");
+        System.out.print(sb);
+        reader.close(); 
     }
 }
