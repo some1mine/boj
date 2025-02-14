9ef3a662f10c34eab1a36c4f6e1a158e7581ba3f
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-02-15 04:09:38 +0900

    [Gold V] Title: 회문, Time: 380 ms, Memory: 48824 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.java
+++ 백준/Gold/17609. 회문/회문.java
@@ -6,17 +6,13 @@ public class Main {
         int n = Integer.parseInt(scanner.nextLine());
         String[] arr = new String[n];
         for (int i = 0; i < n; i++) arr[i] = scanner.nextLine();
-        for (String s : arr) System.out.println(isPalindrome(s, 0));
+        for (String s : arr) System.out.println(isPalindrome(s.toCharArray(), -1, s.length(), 0));
     }
-    private static int isPalindrome(String s, int count) {
+    private static int isPalindrome(char[] arr, int start, int end, int count) {
         if (count > 1) return 2;
-        int start = -1, end = s.length();
-        char[] arr = s.toCharArray();
-        while(++start <= --end) {
+        while (++start <= --end) {
             if (arr[start] != arr[end]) {
-                StringBuilder sb1 = new StringBuilder(s), sb2 = new StringBuilder(s);
-                sb1.deleteCharAt(start); sb2.deleteCharAt(end);
-                if (isPalindrome(sb1.toString(), count + 1) == 0 || isPalindrome(sb2.toString(), count + 1) == 0) return 1;
+                if (isPalindrome(arr, start - 1, end, count + 1) == 0 || isPalindrome(arr, start, end + 1, count + 1) == 0) return 1;
                 return 2;
             }
         }
