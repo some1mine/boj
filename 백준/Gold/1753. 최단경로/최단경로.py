11eab8bd97cad822dfb9b1541053e51bb017cd2b
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-03-13 09:12:07 +0900

    [Gold IV] Title: 최단경로, Time: 540 ms, Memory: 154480 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,4 +1,4 @@
-from queue import PriorityQueue; import sys
+import heapq; import sys
 input = lambda: sys.stdin.readline().rstrip()
 n, e = map(int, input().split()); k = int(input())
 conn = [[] for _ in range(n + 1)]
@@ -6,16 +6,15 @@ for _ in range(e):
     u, v, w = map(int, input().split())
     conn[u].append([v, w])
 
-queue = PriorityQueue()
 dp = [int(1e12) for _ in range(n + 1)]
-dp[k] = 0; queue.put([0, k])
+heap = []; heapq.heappush(heap, [0, k]); dp[k] = 0
 
-while not queue.empty():
-    dist, cur = queue.get()
+while heap:
+    dist, cur = heapq.heappop(heap)
     for i, length in conn[cur]:
         if dist + length < dp[i]:
             dp[i] = dist + length
-            queue.put([dist + length, i])
-            
+            heapq.heappush(heap, [dist + length, i])
+
 for i in dp[1:]:
     print('INF' if i == int(1e12) else i)
