398f11163f703befb6382adf9d88d6cb221b32f1
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-03-27 11:19:02 +0900

    [level 2] Title: 지게차와 크레인, Time: 39.44 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -8,35 +8,35 @@ class Solution {
         for(String s : requests) answer -= calculate(store, s.charAt(0), s.length() == 2);
         return answer;
     }
-    int calculate(char[][] storage, char request, boolean isAll) {
-        int answer = 0; boolean[][] visited = new boolean[storage.length][storage[0].length];
+    int calculate(char[][] store, char request, boolean isAll) {
+        int answer = 0; boolean[][] visited = new boolean[store.length][store[0].length];
         if(isAll) {
-            for(int i = 0 ; i < storage.length ; i++) {
-                for(int j = 0 ; j < storage[0].length ; j++) {
-                    if(storage[i][j] == request) {
-                        answer++; storage[i][j] = '\0';
+            for(int i = 0 ; i < store.length ; i++) {
+                for(int j = 0 ; j < store[0].length ; j++) {
+                    if(store[i][j] == request) {
+                        answer++; store[i][j] = '\0';
                     }
                 }
             }
             return answer;
         }
-        for(int i = 0 ; i < storage.length ; i++) answer += bfs(storage, new int[]{i, 0}, new int[]{i, storage[0].length - 1}, request, visited);
-        for(int i = 0 ; i < storage[0].length ; i++) answer += bfs(storage, new int[]{0, i}, new int[]{storage.length - 1, i}, request, visited);
+        for(int i = 0 ; i < store.length ; i++) answer += bfs(store, new int[]{i, 0}, new int[]{i, store[0].length - 1}, request, visited);
+        for(int i = 0 ; i < store[0].length ; i++) answer += bfs(store, new int[]{0, i}, new int[]{store.length - 1, i}, request, visited);
         return answer;
     }
-    int bfs(char[][] storage, int[] a, int[] b, char request, boolean[][] visited) {
+    int bfs(char[][] store, int[] a, int[] b, char request, boolean[][] visited) {
         int answer = 0; Queue<int[]> queue = new LinkedList<>(); queue.offer(a); queue.offer(b);
         while(!queue.isEmpty()) {
             int[] cur = queue.poll(); int y = cur[0], x = cur[1];
-            if(storage[y][x] == '\0' && !visited[y][x]) {
+            if(store[y][x] == '\0' && !visited[y][x]) {
                 for(int[] d : directions) {
                     int dy = y + d[0], dx = x + d[1];
-                    if(dy < 0 || dy >= storage.length || dx < 0 || dx >= storage[0].length) continue;
+                    if(dy < 0 || dy >= store.length || dx < 0 || dx >= store[0].length) continue;
                     queue.offer(new int[]{dy, dx});
                 }
             }
-            if(storage[y][x] == request) {
-                answer++; storage[y][x] = '\0';
+            if(store[y][x] == request) {
+                answer++; store[y][x] = '\0';
             }
             visited[y][x] = true;
         }
