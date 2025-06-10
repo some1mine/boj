a6bc137dbdfd1a1f1aad9f9050e85fe98c942f41
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-06-11 05:15:49 +0900

    [level 3] Title: 양과 늑대, Time: 0.38 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -8,14 +8,14 @@ def solution(info, edges):
     queue.append(({0}, 1, 0)) # 방문 정점, 양의 수, 늑대 수
     while queue:
         curSet, curSheep, curWolf = queue.popleft() 
-        ans = max(ans, curSheep); nxt = set()
+        ans = max(ans, curSheep); nxt = set() # 최대값 갱신
         for node in curSet:
             for adj in lst[node]: # 방문한 정점들에 연결된 정점 중 방문하지 않은 정점들을 모아
                 if adj not in curSet: nxt.add(adj)
         for node in nxt:
-            nxtSet = curSet | {node} # 다음 방문 정점을 추가
+            nxtSet = curSet | {node} # 가능 여부를 따져 정점들, queue에 양의 수, 늑대 수와 함께 추가
             if info[node] == 0:
-                if tuple(sorted((nxtSet))) not in visited: # queue에 추가 작업, visited에는 immutable type
+                if tuple(sorted((nxtSet))) not in visited: # visited에는 immutable type으로 멤버 연산
                     queue.append((nxtSet, curSheep + 1, curWolf))
                     visited.add(tuple(sorted((nxtSet))))
             elif info[node] == 1 and curSheep > curWolf + 1:
