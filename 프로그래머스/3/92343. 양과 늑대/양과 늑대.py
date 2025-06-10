d22fb4b266a866ec03804c51b09bfb00f97dd2a4
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-06-11 05:13:40 +0900

    [level 3] Title: 양과 늑대, Time: 0.35 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -2,21 +2,20 @@ from collections import deque
 
 def solution(info, edges):
     lst = [[] for _ in range(len(info))]
-    for a, b in edges:
+    for a, b in edges: # 간선들을 배열에 담아
         lst[a].append(b); lst[b].append(a)
     visited = set(); queue = deque(); ans = 0
-    queue.append(({0}, 1, 0))
+    queue.append(({0}, 1, 0)) # 방문 정점, 양의 수, 늑대 수
     while queue:
-        curSet, curSheep, curWolf = queue.popleft()
-        ans = max(ans, curSheep)
-        nxt = set()
+        curSet, curSheep, curWolf = queue.popleft() 
+        ans = max(ans, curSheep); nxt = set()
         for node in curSet:
-            for adj in lst[node]:
+            for adj in lst[node]: # 방문한 정점들에 연결된 정점 중 방문하지 않은 정점들을 모아
                 if adj not in curSet: nxt.add(adj)
         for node in nxt:
-            nxtSet = curSet | {node}
+            nxtSet = curSet | {node} # 다음 방문 정점을 추가
             if info[node] == 0:
-                if tuple(sorted((nxtSet))) not in visited:
+                if tuple(sorted((nxtSet))) not in visited: # queue에 추가 작업, visited에는 immutable type
                     queue.append((nxtSet, curSheep + 1, curWolf))
                     visited.add(tuple(sorted((nxtSet))))
             elif info[node] == 1 and curSheep > curWolf + 1:
