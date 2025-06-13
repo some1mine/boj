a7500d3d087a5274ca3fad8da6abead4aac45d40
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-06-13 20:59:10 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 45.11 ms, Memory: 224 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -3,9 +3,9 @@ class Solution {
         int r = board.length, c = board[0].length, answer = r * c;
         int[][] dp = new int[r + 2][c + 2];
         for(int[] s : skill) {
-            int amount = (s[0] == 1) ? -s[5] : s[5];
-            dp[s[1] + 1][s[2] + 1] += amount; dp[s[1] + 1][s[4] + 2] -= amount;
-            dp[s[3] + 2][s[2] + 1] -= amount; dp[s[3] + 2][s[4] + 2] += amount;
+            s[5] *= s[0] == 1 ? -1 : 1;
+            dp[s[1] + 1][s[2] + 1] += s[5]; dp[s[1] + 1][s[4] + 2] -= s[5];
+            dp[s[3] + 2][s[2] + 1] -= s[5]; dp[s[3] + 2][s[4] + 2] += s[5];
         }
         for(int i = 1 ; i <= r ; i++) {
             for(int j = 1 ; j <= c ; j++) dp[i][j] += dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1];
