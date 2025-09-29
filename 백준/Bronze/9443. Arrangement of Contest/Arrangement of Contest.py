dfa3f4df061085423c4501aa1254fd75bd2b34d7
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-09-29 13:58:13 +0900

    [Bronze III] Title: Arrangement of Contest, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/9443. Arrangement of Contest/Arrangement of Contest.py
+++ 백준/Bronze/9443. Arrangement of Contest/Arrangement of Contest.py
@@ -1,4 +1,3 @@
-n = int(input()); ans = 0; lst = []; stand = ord('A')
-for _ in range(n): lst.append(ord(input()[0]) - stand); lst.sort()
+n = int(input()); ans = 0; stand = ord('A'); lst = sorted(ord(input()[0]) - stand for _ in range(n))
 for i in range(n): ans += lst[i] == ans
 print(ans)
