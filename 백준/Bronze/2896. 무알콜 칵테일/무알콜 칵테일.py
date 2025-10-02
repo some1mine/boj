7998bf2e973c17cba9fc7a90ffec612241c5ee1f
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-10-02 12:54:38 +0900

    [Bronze I] Title: 무알콜 칵테일, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2896. 무알콜 칵테일/무알콜 칵테일.py
+++ 백준/Bronze/2896. 무알콜 칵테일/무알콜 칵테일.py
@@ -1,4 +1,4 @@
-a, b, c = map(int, input().split()); i, j, k = map(int, input().split()); t = min(a / i, b / j, c / k)
-print(f"{a - i * t:.6f}" if (a - i * t) % 1 else a - i * t, 
-      f'{b - j * t:.6f}' if (b - j * t) % 1 else b - j * t, 
-      f"{c - k * t:.6f}" if (c - k * t) % 1 else c - k * t)
+a, b, c = map(int, input().split()); i, j, k = map(int, input().split()); d = min(a / i, b / j, c / k)
+print(f"{a - i * d:.6f}" if (a - i * d) % 1 else a - i * d, 
+      f'{b - j * d:.6f}' if (b - j * d) % 1 else b - j * d, 
+      f"{c - k * d:.6f}" if (c - k * d) % 1 else c - k * d)
