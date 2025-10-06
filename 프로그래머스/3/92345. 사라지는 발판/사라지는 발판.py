d4e21842f76d53c06b538f3bb87ca7fdf9abac8d
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-10-06 22:46:11 +0900

    [level 3] Title: 사라지는 발판, Time: 2549.01 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
@@ -3,22 +3,16 @@ from copy import deepcopy
 dy = [-1, 0, 1, 0]; dx = [0, 1, 0, -1]
 def in_range(y, x, board): return 0 <= y < len(board) and 0 <= x < len(board[0])
 def func(board, cury, curx, opy, opx):
-    cnt = 0
-    
-    if not in_range(cury, curx, board) or board[cury][curx] == 0: return False, 0
+    cnt = sum((not in_range(cury + dy[i], curx + dx[i], board) or board[cury + dy[i]][curx + dx[i]] == 0) for i in range(4))
+    if not in_range(cury, curx, board) or board[cury][curx] == 0 or cnt == 4: return False, 0
     if not in_range(opy, opx, board) or board[opy][opx] == 0: return True, 0
-
-    for i in range(4):
-        ny = cury + dy[i]; nx = curx + dx[i]; cnt += not in_range(ny, nx, board) or board[ny][nx] == 0
-    if cnt == 4: return False, 0
     
     cboard = deepcopy(board); cboard[cury][curx] = 0;  win_case = []; lose_case = []
     for i in range(4):
         ny = cury + dy[i]; nx = curx + dx[i]; nres, ncnt = func(cboard, opy, opx, ny, nx)
         if nres == False: win_case.append(ncnt)
         else: lose_case.append(ncnt)
-    if win_case: return True, min(win_case) + 1
-    return False, max(lose_case) + 1
+    return (True, min(win_case) + 1) if win_case else (False, max(lose_case) + 1)
 
 
 def solution(board, aloc, bloc):
