499274f63bf0c31c8e48fec0b70ae80ea2628ed7
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-10-06 22:31:58 +0900

    [level 3] Title: 사라지는 발판, Time: 2118.30 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
@@ -0,0 +1,32 @@
+from copy import deepcopy
+
+dy = [-1, 0, 1, 0]; dx = [0, 1, 0, -1]
+
+def in_range(y, x): return 0 <= y < r and 0 <= x < c
+
+def func(board, cury, curx, opy, opx):
+    global r, c
+    
+    if not in_range(cury, curx) or board[cury][curx] == 0: return False, 0
+    if not in_range(opy, opx) or board[opy][opx] == 0: return True, 0
+    
+    cnt = 0
+    for i in range(4):
+        ny = cury + dy[i]; nx = curx + dx[i]
+        cnt += not in_range(ny, nx) or board[ny][nx] == 0
+    if cnt == 4: return (False, 0)
+    
+    cboard = deepcopy(board); cboard[cury][curx] = 0;  win_case = []; lose_case = []
+    for i in range(4):
+        ny = cury + dy[i]; nx = curx + dx[i];  nres, ncnt = func(cboard, opy, opx, ny, nx)
+        if nres == False: win_case.append(ncnt)
+        else: lose_case.append(ncnt)
+    if win_case: return True, min(win_case) + 1
+    else: return False, max(lose_case) + 1
+
+
+def solution(board, aloc, bloc):
+    global r, c
+    r = len(board); c = len(board[0])
+    
+    return func(board, *aloc, *bloc)[1]
