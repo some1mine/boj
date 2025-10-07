e9efa7beb0c30809a83ca4768082c137d1125b5d
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-10-08 02:13:12 +0900

    [level 3] Title: 사라지는 발판, Time: 2537.32 ms, Memory: 9.46 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
@@ -5,7 +5,7 @@ dy = [-1, 0, 1, 0]; dx = [0, 1, 0, -1]
 def out_range(y, x, board): return not 0 <= y < len(board) or not 0 <= x < len(board[0])
 
 def func(board, cury, curx, opy, opx):
-    cnt = sum((out_range(cury + dy[i], curx + dx[i], board) or board[cury + dy[i]][curx + dx[i]] == 0) for i in range(4))
+    cnt = sum(out_range(cury + dy[i], curx + dx[i], board) or board[cury + dy[i]][curx + dx[i]] == 0 for i in range(4))
     
     if out_range(cury, curx, board) or board[cury][curx] == 0 or cnt == 4: return False, 0
     if out_range(opy, opx, board) or board[opy][opx] == 0: return True, 0
