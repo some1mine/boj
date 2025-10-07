7083a15628d53c8f3128ae01dc921a0f741d576f
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-10-08 02:11:32 +0900

    [level 3] Title: 사라지는 발판, Time: 2532.22 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/README.md
+++ 프로그래머스/3/92345. 사라지는 발판/README.md
@@ -1,10 +1,10 @@
 # [level 3] 사라지는 발판 - 92345 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92345?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92345) 
 
 ### 성능 요약
 
-메모리: 9.49 MB, 시간: 2635.62 ms
+메모리: 9.23 MB, 시간: 2532.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 06일 22:55:05
+2025년 10월 08일 02:11:16
 
 ### 문제 설명
 
--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
@@ -1,14 +1,24 @@
-from copy import deepcopy; dy = [-1, 0, 1, 0]; dx = [0, 1, 0, -1]
-def in_range(y, x, board): return 0 <= y < len(board) and 0 <= x < len(board[0])
+from copy import deepcopy; 
+
+dy = [-1, 0, 1, 0]; dx = [0, 1, 0, -1]
+
+def out_range(y, x, board): return not 0 <= y < len(board) or not 0 <= x < len(board[0])
+
 def func(board, cury, curx, opy, opx):
-    cnt = sum((not in_range(cury + dy[i], curx + dx[i], board) or board[cury + dy[i]][curx + dx[i]] == 0) for i in range(4))
-    if not in_range(cury, curx, board) or board[cury][curx] == 0 or cnt == 4: return False, 0
-    if not in_range(opy, opx, board) or board[opy][opx] == 0: return True, 0
-    cboard = deepcopy(board); cboard[cury][curx] = 0; win_case = []; lose_case = []
+    cnt = sum((out_range(cury + dy[i], curx + dx[i], board) or board[cury + dy[i]][curx + dx[i]] == 0) for i in range(4))
+    
+    if out_range(cury, curx, board) or board[cury][curx] == 0 or cnt == 4: return False, 0
+    if out_range(opy, opx, board) or board[opy][opx] == 0: return True, 0
+
+    copied = deepcopy(board); copied[cury][curx] = 0; win_case = []; lose_case = []
+    
     for i in range(4):
-        ny = cury + dy[i]; nx = curx + dx[i]; nres, ncnt = func(cboard, opy, opx, ny, nx)
-        if nres == False: win_case.append(ncnt)
-        else: lose_case.append(ncnt)
-    return (True, min(win_case) + 1) if win_case else (False, max(lose_case) + 1)
+        ny = cury + dy[i]; nx = curx + dx[i]; nres, ncnt = func(copied, opy, opx, ny, nx)
+        if nres: lose_case.append(ncnt)
+        else: win_case.append(ncnt)
+        
+    if win_case: return True, min(win_case) + 1
+    else: return False, max(lose_case) + 1
+
 def solution(board, aloc, bloc):
     return func(board, *aloc, *bloc)[1]
