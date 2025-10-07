ac2850e05497bc66347f5d63bbf530f14e049f5c
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-10-08 02:16:50 +0900

    [level 3] Title: 사라지는 발판, Time: 2634.06 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
@@ -5,9 +5,10 @@ dy = [-1, 0, 1, 0]; dx = [0, 1, 0, -1]
 def out_range(y, x, board): return 0 > y or y >= len(board) or 0 > x or x >= len(board[0])
 
 def func(board, cury, curx, opy, opx):
-    cnt = sum(out_range(cury + dy[i], curx + dx[i], board) or board[cury + dy[i]][curx + dx[i]] == 0 for i in range(4))
     
-    if out_range(cury, curx, board) or board[cury][curx] == 0 or cnt == 4: return False, 0
+    if out_range(cury, curx, board) or board[cury][curx] == 0 or \
+        sum(out_range(cury + dy[i], curx + dx[i], board) or board[cury + dy[i]][curx + dx[i]] == 0 \
+            for i in range(4)) == 4: return False, 0
     if out_range(opy, opx, board) or board[opy][opx] == 0: return True, 0
 
     copied = deepcopy(board); copied[cury][curx] = 0; win_case = []; lose_case = []
