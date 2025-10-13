8cfee3e1e5d040f83b920afe1aaff88e185ea44b
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-10-14 00:29:03 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 2.41 ms, Memory: 9.59 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -11,4 +11,5 @@ def solution(n, t, m, timetable):
     for i in range(n):
         boarded = [crews[j] for j in range(idx, len(timetable)) if crews[j] <= basetime + i * t][:m]
         idx += len(boarded); boarded_times.append(list(sorted(boarded)))
+        
     return __str__(basetime + (n - 1) * t) if len(boarded_times[-1]) < m else __str__(boarded_times[-1][-1] - 1)
