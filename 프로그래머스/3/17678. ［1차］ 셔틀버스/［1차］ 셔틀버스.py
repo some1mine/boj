52b2a8875f431e21d05efb5e5947bb798c606bc9
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-10-14 00:19:59 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 1.37 ms, Memory: 9.62 MB -BaekjoonHub

---
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -0,0 +1,18 @@
+def to_min(time_str):
+    h, m = map(int, time_str.split(':'))
+    return h * 60 + m
+
+def __str__(time):
+    h = time // 60; m = time % 60
+    return f'{h:02d}:{m:02d}'
+
+def solution(n, t, m, timetable):
+    crews = sorted(to_min(x) for x in timetable)
+    basetime = 9 * 60; idx = 0; boarded_times = []
+
+    for i in range(n):
+        arrive = basetime + i * t; boarded = []
+        while idx < len(crews) and crews[idx] <= arrive and len(boarded) < m:
+            boarded.append(crews[idx]); idx += 1
+        boarded_times.append(list(sorted(boarded)))
+    return __str__(basetime + (n - 1) * t) if len(boarded_times[-1]) < m else __str__(boarded_times[-1][-1] - 1)
