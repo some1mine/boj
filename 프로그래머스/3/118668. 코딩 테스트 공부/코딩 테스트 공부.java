8149e5a39714bcde5dfbdae1f87777b1cfc445d8
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2026-01-12 15:04:55 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 15.14 ms, Memory: 54.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int alp, int cop, int[][] problems) {
+        int targetAlp = -1, targetCop = -1;
+        for(int[] p : problems) {
+            targetAlp = targetAlp < p[0] ? p[0] : targetAlp;
+            targetCop = targetCop < p[1] ? p[1] : targetCop;
+        }
+        targetAlp = Math.max(alp, targetAlp); targetCop = Math.max(cop, targetCop);
+        int[][] targets = Arrays.copyOf(problems, problems.length + 2);
+        targets[targets.length - 1] = new int[]{0,0,1,0,1};
+        targets[targets.length - 2] = new int[]{0,0,0,1,1};
+        
+        int[][] dp = new int[targetAlp + 2][targetCop + 2];
+        for(int[] d : dp) Arrays.fill(d, 9999999); dp[alp][cop] = 0;
+        
+        for(int i = alp ; i < targetAlp + 1 ; i++) {
+            for(int j = cop ; j < targetCop + 1 ; j++) {
+                for(int[] p : targets) {
+                    if(i >= p[0] && j >= p[1]) {
+                        int nextAlp = Math.min(targetAlp, i + p[2]), nextCop = Math.min(targetCop, j + p[3]);
+                        dp[nextAlp][nextCop] = Math.min(dp[nextAlp][nextCop], dp[i][j] + p[4]);
+                    }
+                }
+            }
+        }
+        return dp[targetAlp][targetCop];
+    }
+}
