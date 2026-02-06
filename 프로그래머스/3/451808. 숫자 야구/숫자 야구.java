baf0d55cdfa7b2e6e7d8844d16e26b0dba90de98
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2026-02-06 11:04:30 +0900

    [level 3] Title: 숫자 야구, Time: 36.35 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/3/451808. 숫자 야구/숫자 야구.java
+++ 프로그래머스/3/451808. 숫자 야구/숫자 야구.java
@@ -1,64 +1,36 @@
-import java.util.function.Function;
 import java.util.*;
+import java.util.function.Function;
 
 class Solution {
+    public String[] candi = new String[3024]; int idx = 0;
     public int solution(int n, Function<Integer, String> submit) {
-        List<Integer> list = new ArrayList<>();
-        insertAllCase(list);
-
-        while (list.size() != 1) {
-            int num = list.get(0);
-            String result = submit.apply(num);
-            list = filterList(list, num, result.charAt(0) - '0', result.charAt(3) - '0');
+        init("", 0); String result = "0S 0B";
+        while(candi.length > 1) {
+            result = submit.apply(Integer.parseInt(candi[0]));
+            candi = filter(candi[0], result.charAt(0) - '0', result.charAt(3) - '0');
         }
-
-        return list.get(0);
+        return Integer.parseInt(candi[0]);
     }
-
-    // 3024개 전체 경우 생성
-    private void insertAllCase(List<Integer> list) {
-        for (int a = 1; a <= 9; a++) {
-            for (int b = 1; b <= 9; b++) {
-                if (a == b) continue;
-                for (int c = 1; c <= 9; c++) {
-                    if (a == c || b == c) continue;
-                    for (int d = 1; d <= 9; d++) {
-                        if (a == d || b == d || c == d) continue;
-                        list.add(1000 * a + 100 * b + 10 * c + d);
-                    }
-                }
-            }
+    public void init(String num, int depth) {
+        if(depth == 4) {
+            candi[idx] = num; idx++; return;
+        }
+        for(int i = 1 ; i < 10 ; i++) {
+            if(num.contains(Integer.toString(i))) continue;
+            String newNum = num + i; init(newNum, depth + 1);
         }
     }
-
-    // submit 결과에 따른 list 업데이트
-    private List<Integer> filterList(List<Integer> list, int num, int strike, int ball) {
-        List<Integer> newList = new ArrayList<>();
-        String strNum = Integer.toString(num);
-        Set<Character> numSt = new HashSet<>();
-        numSt.add(strNum.charAt(0));
-        numSt.add(strNum.charAt(1));
-        numSt.add(strNum.charAt(2));
-        numSt.add(strNum.charAt(3));
-
-        for (int ele : list) {
-            String strEle = Integer.toString(ele);
-            int strikeCnt = 0;
-            int ballCnt = 0;
-
-            for (int i = 0; i < 4; i++) {
-                if (strNum.charAt(i) == strEle.charAt(i)) {
-                    strikeCnt++;
-                } else if (numSt.contains(strEle.charAt(i))) {
-                    ballCnt++;
-                }
-            }
-
-            if (strikeCnt == strike && ballCnt == ball) {
-                newList.add(ele);
-            }
+    public String[] filter(String num, int strike, int ball) {
+        return Arrays.stream(candi)
+            .filter(c -> match(num.toCharArray(), c.toCharArray(), strike, ball))
+            .toArray(String[]::new);
+    }
+    public boolean match(char[] num, char[] candi, int strike, int ball) {
+        int sCnt = 0, bCnt = 0; Set<Character> tmp = Set.of(num[0], num[1], num[2], num[3]);
+        for(int i = 0 ; i < 4 ; i++) {
+            if(num[i] == candi[i]) sCnt++;
+            else if(tmp.contains(candi[i])) bCnt++;
         }
-
-        return newList;
+        return sCnt == strike && bCnt == ball;
     }
 }
