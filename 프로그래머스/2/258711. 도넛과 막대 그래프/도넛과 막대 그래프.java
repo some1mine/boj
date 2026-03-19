7e8dca119ebf2e6ba5ea88f83a3c1f919588110a
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2026-03-19 20:34:56 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 114.02 ms, Memory: 203 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i = 1 ; i <= max ; i++) {
             if(conn[i][0] == 0 && conn[i][1] >= 2) answer[0] = i;
             if(conn[i][0] >= 1 && conn[i][1] == 0) answer[2]++;
-            if(conn[i][0] >= 2 && conn[i][1] >= 2) answer[3]++;
+            if(conn[i][0] > 1 && conn[i][1] > 1) answer[3]++;
         }
         answer[1] = conn[answer[0]][1] - answer[2] - answer[3];
         return answer;
