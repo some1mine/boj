79549077710ab90e4e2163f6895f96662469c537
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2026-03-19 20:41:48 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 172.17 ms, Memory: 216 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -1,13 +1,10 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[][] edges) {
-        Set<Integer> nums = new HashSet<>(); int[] answer = new int[4]; 
-        int[][] conn = new int[1_000_001][2];
+        int[] answer = new int[4]; int[][] conn = new int[1_000_001][2];
         for(int[] edge : edges) {
-            conn[edge[0]][1]++; conn[edge[1]][0]++; nums.add(edge[0]); nums.add(edge[1]);
+            conn[edge[0]][1]++; conn[edge[1]][0]++;
         }
-        for(int i : nums) {
+        for(int i = 1 ; i <= 1_000_000 ; i++) {
             if(conn[i][0] == 0 && conn[i][1] >= 2) answer[0] = i;
             if(conn[i][0] >= 1 && conn[i][1] == 0) answer[2]++;
             if(conn[i][0] > 1 && conn[i][1] > 1) answer[3]++;
