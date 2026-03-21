5f528cd941b588c863d46585bf778b8bfa6ae706
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2026-03-21 09:09:15 +0900

    [level 3] Title: 산 모양 타일링, Time: 16.49 ms, Memory: 95.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int[] tops) {
+        int[][] answers = new int[n][2]; answers[0][0] = tops[0] + 2; answers[0][1] = 1;
+        for (int i = 1 ; i < n ; i++) {
+            answers[i][1] = (answers[i - 1][0] + answers[i - 1][1]) % 10007;
+            answers[i][0] = ((tops[i] + 2) * answers[i - 1][0] + (tops[i] + 1) * answers[i - 1][1]) % 10007;
+        }
+        return (answers[n - 1][0] + answers[n - 1][1]) % 10007;
+    }
+}
