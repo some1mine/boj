8506da128e529ccc906df9f8bdb09505552d3c8d
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2026-03-23 21:31:29 +0900

    [level 3] Title: 주사위 고르기, Time: 1618.97 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -1,24 +1,25 @@
 from itertools import combinations
 
-def get_win_num(dice, comb1, comb2):
-    dp1 = [[0] * (501) for _ in range(len(comb1) + 1)]; dp1[0][0] = 1
-    dp2 = [[0] * (501) for _ in range(len(comb1) + 1)]; dp2[0][0] = 1
+def get_win_num(dice, comb1, comb2): 
+    n = len(comb1)
+    dp1 = [[0] * (501) for _ in range(n + 1)]; dp1[0][0] = 1
+    dp2 = [[0] * (501) for _ in range(n + 1)]; dp2[0][0] = 1
     
-    for n in range(1, len(comb1) + 1):
+    for i in range(1, len(comb1) + 1):
         for x in range(1, 501):
-            for num in dice[comb1[n - 1]]: dp1[n][x] += dp1[n - 1][x - num] if x - num >= 0 else 0
-            for num in dice[comb2[n - 1]]: dp2[n][x] += dp2[n - 1][x - num] if x - num >= 0 else 0
+            for num in dice[comb1[i - 1]]: dp1[i][x] += dp1[i - 1][x - num] if x - num >= 0 else 0
+            for num in dice[comb2[i - 1]]: dp2[i][x] += dp2[i - 1][x - num] if x - num >= 0 else 0
     
     dp2_psum = [0] * 501; win_num = 0
-    for x in range(1, 501): dp2_psum[x] = dp2[len(comb1)][x] + dp2_psum[x - 1]
-    for x in range(1, 501): win_num += dp1[len(comb1)][x] * dp2_psum[x - 1]
+    for x in range(1, 501): dp2_psum[x] = dp2[n][x] + dp2_psum[x - 1]
+    for x in range(1, 501): win_num += dp1[n][x] * dp2_psum[x - 1]
 
     return win_num
 
 def solution(dice):
-    best_win_num = -1; best_case = None
-    for cur_case in combinations(range(len(dice)), len(dice) // 2):
-        cur_win_num = get_win_num(dice, cur_case, tuple(sorted(list(set(range(len(dice))) - set(cur_case)))))
+    best_win_num = -1; best_case = None; n = len(dice) // 2
+    for cur_case in combinations(range(n * 2), n):
+        cur_win_num = get_win_num(dice, cur_case, tuple(sorted(list(set(range(n * 2)) - set(cur_case)))))
         if cur_win_num > best_win_num:
             best_win_num = cur_win_num; best_case = list(cur_case)
     
