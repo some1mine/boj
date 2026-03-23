0b288e9ca832f703a2bda367f2eec1239becaec4
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2026-03-23 21:20:20 +0900

    [level 3] Title: 주사위 고르기, Time: 1449.04 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -1,6 +1,6 @@
 from itertools import combinations
 
-def get_win_num(comb1, comb2):
+def get_win_num(dice, comb1, comb2):
     dp1 = [[0] * (501) for _ in range(len(comb1) + 1)]; dp1[0][0] = 1
     dp2 = [[0] * (501) for _ in range(len(comb1) + 1)]; dp2[0][0] = 1
     
@@ -15,12 +15,10 @@ def get_win_num(comb1, comb2):
 
     return win_num
 
-def solution(_dice):
-    global dice; dice = _dice
-    
+def solution(dice):
     best_win_num = -1; best_case = None
     for cur_case in combinations(range(len(dice)), len(dice) // 2):
-        cur_win_num = get_win_num(cur_case, tuple(sorted(list(set(range(len(dice))) - set(cur_case)))))
+        cur_win_num = get_win_num(dice, cur_case, tuple(sorted(list(set(range(len(dice))) - set(cur_case)))))
         if cur_win_num > best_win_num:
             best_win_num = cur_win_num; best_case = list(cur_case)
     
