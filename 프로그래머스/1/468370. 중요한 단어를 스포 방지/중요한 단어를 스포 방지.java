847cdc17945541587080560526ecb9e5a9dfe5eb
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2026-03-26 21:15:31 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 9.77 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
@@ -3,24 +3,21 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(String message, int[][] spoiler_ranges) {
-        int answer = 0; boolean[] isSpoiler = new boolean[message.length()];
+        int answer = 0, n = message.length(); boolean[] isSpoiler = new boolean[n];
         List<int[]> words = new ArrayList<>(); Set<String> others = new HashSet<>();
         for (int[] spoiler : spoiler_ranges) IntStream.range(spoiler[0], spoiler[1] + 1).forEach(i -> isSpoiler[i] = true);
-
-        for(int i = 0 ; i < message.length() ; i++) {
+        for(int i = 0 ; i < n ; i++) {
             if(message.charAt(i) == ' ') continue;
             int start = i;
-            while(i < message.length() && message.charAt(i) != ' ') i++;
+            while(i < n && message.charAt(i) != ' ') i++;
             words.add(new int[]{start, --i});
         }
-
         for (int[] w : words) {
             boolean contains = IntStream.range(w[0], w[1] + 1).anyMatch(i -> isSpoiler[i]);
             if (!contains) others.add(message.substring(w[0], w[1] + 1));
         }
-
         for (int[] spoiler : spoiler_ranges) {
-            for(int i = spoiler[0] ; i <= spoiler[1] ; i++) {
+            for (int i = spoiler[0]; i <= spoiler[1]; i++) {
                 while(message.charAt(i) == ' ') i++;
                 int start = i;
                 while(start > 0 && message.charAt(start - 1) != ' ') start--;
