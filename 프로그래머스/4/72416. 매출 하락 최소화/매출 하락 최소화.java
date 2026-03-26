f8abc7671c89a3b76861ecbfdca768d7682d741f
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2026-03-27 07:56:49 +0900

    [level 4] Title: 매출 하락 최소화, Time: 147.89 ms, Memory: 157 MB -BaekjoonHub

---
+++ 프로그래머스/4/72416. 매출 하락 최소화/매출 하락 최소화.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[][] dp; int[] sales; List<Integer>[] graph;
+    public int solution(int[] sales, int[][] links) {
+        this.sales = sales; dp = new int[sales.length + 1][2]; graph = new ArrayList[sales.length + 1];
+        for(int i = 0 ; i<= sales.length ; i++) graph[i] = new ArrayList<>();
+        for(int[] link : links) graph[link[0]].add(link[1]); dfs(1);
+        return Math.min(dp[1][1], dp[1][0]);
+    }
+    public void dfs(int v) {
+        dp[v][1] = sales[v - 1]; dp[v][0] = 0; boolean flag = false; int temp = Integer.MAX_VALUE;
+        for(int child : graph[v]){
+            dfs(child); int min = Math.min(dp[child][0], dp[child][1]);
+            dp[v][1] += min; dp[v][0] += min; flag = dp[child][1] <= dp[child][0] ? true : flag;
+            temp = Math.min(temp, dp[child][1] - dp[child][0]);  
+        }
+        dp[v][0] += !flag && !graph[v].isEmpty() ? temp : 0;
+    }
+}
