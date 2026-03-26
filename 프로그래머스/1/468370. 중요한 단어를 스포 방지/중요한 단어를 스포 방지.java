711fc320ea3f53e6d5b7a9c231ed3409feaa7af5
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2026-03-26 20:42:25 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 8.54 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
@@ -1,78 +1,36 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(String message, int[][] spoiler_ranges) {
-        int n = message.length();
-        boolean[] isSpoiler = new boolean[n];
+        int answer = 0; boolean[] isSpoiler = new boolean[message.length()];
+        List<int[]> words = new ArrayList<>(); Set<String> others = new HashSet<>();
+        for (int[] spoiler : spoiler_ranges) IntStream.range(spoiler[0], spoiler[1] + 1).forEach(i -> isSpoiler[i] = true);
 
-        // 1. 스포 구간 표시
-        for (int[] r : spoiler_ranges) {
-            for (int i = r[0]; i <= r[1]; i++) {
-                isSpoiler[i] = true;
-            }
-        }
-
-        // 2. 전체 단어의 시작/끝 인덱스 구하기
-        List<int[]> words = new ArrayList<>();
-        int i = 0;
-        while (i < n) {
-            if (message.charAt(i) == ' ') {
-                i++;
-                continue;
-            }
+        for(int i = 0 ; i < message.length() ; i++) {
+            if(message.charAt(i) == ' ') continue;
             int start = i;
-            while (i < n && message.charAt(i) != ' ') i++;
-            int end = i - 1;
-            words.add(new int[]{start, end});
+            while(i < message.length() && message.charAt(i) != ' ') i++;
+            words.add(new int[]{start, --i});
         }
 
-        // 3. 스포 바깥에서 등장한 단어 수집
-        Set<String> nonSpoilerWords = new HashSet<>();
         for (int[] w : words) {
-            boolean hasSpoiler = false;
-            for (int j = w[0]; j <= w[1]; j++) {
-                if (isSpoiler[j]) {
-                    hasSpoiler = true;
-                    break;
-                }
-            }
-            if (!hasSpoiler) {
-                String word = message.substring(w[0], w[1] + 1);
-                nonSpoilerWords.add(word);
-            }
+            boolean contains = IntStream.range(w[0], w[1] + 1).anyMatch(i -> isSpoiler[i]);
+            if (!contains) others.add(message.substring(w[0], w[1] + 1));
         }
 
-        // 4. 스포 구간 순서대로 처리
-        int answer = 0;
-        Set<String> seen = new HashSet<>();
-
-        for (int[] r : spoiler_ranges) {
-            int idx = r[0];
-
-            while (idx <= r[1]) {
-                if (message.charAt(idx) == ' ') {
-                    idx++;
-                    continue;
+        for (int[] spoiler : spoiler_ranges) {
+            for(int i = spoiler[0] ; i <= spoiler[1] ; i++) {
+                while(message.charAt(i) == ' ') i++;
+                int start = i;
+                while(start > 0 && message.charAt(start - 1) != ' ') start--;
+                while(i < message.length() - 1 && message.charAt(i + 1) != ' ') i++;
+                String word = message.substring(start, i + 1);
+                if(!others.contains(word)) {
+                    answer++; others.add(word);
                 }
-
-                // 단어 찾기
-                int start = idx;
-                while (start > 0 && message.charAt(start - 1) != ' ') start--;
-                int end = idx;
-                while (end < n - 1 && message.charAt(end + 1) != ' ') end++;
-
-                String word = message.substring(start, end + 1);
-
-                // 조건 검사
-                if (!nonSpoilerWords.contains(word) && !seen.contains(word)) {
-                    answer++;
-                    seen.add(word);
-                }
-
-                idx = end + 1;
             }
         }
-
         return answer;
     }
 }
