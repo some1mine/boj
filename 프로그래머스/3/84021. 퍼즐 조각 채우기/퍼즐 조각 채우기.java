57b659318a9cf98e925169a20989a116b1dfed04
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2026-04-14 14:44:47 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 133.00 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    public int solution(int[][] game_board, int[][] table) {
+        int answer = 0; List<int[][]> block = collect(table, 1), target = collect(game_board, 0);
+        boolean[] visited1 = new boolean[block.size()], visited2 = new boolean[target.size()];
+        for(int i = 0 ; i < block.size() ; i++) {
+            int[][] t = block.get(i);
+            for(int j = 0 ; j < target.size() ; j++) {
+                int[][] b = target.get(j);
+                for(int k = 0 ; k < 4 ; k++) {
+                    if(!visited1[i] && !visited2[j] && isSame(t, b)) {
+                        answer += (int) Arrays.stream(t).flatMapToInt(Arrays::stream).sum(); 
+                        visited1[i] = true; visited2[j] = true; break;
+                    }
+                    t = rotate(t);
+                }
+            }
+        }
+        for(int[][] b : target) System.out.println(Arrays.deepToString(b));
+        return answer;
+    }
+    public boolean isSame(int[][] t, int[][] b) {
+        if(t.length != b.length || t[0].length != b[0].length) return false;
+        for(int i = 0 ; i < t.length ; i++) {
+            for(int j = 0 ; j < b.length ; j++) {
+                if(t[i][j] != b[i][j]) return false;
+            }
+        }
+        return true;
+    }
+    public List<int[][]> collect(int[][] table, int target) {
+        List<int[][]> answer = new ArrayList<>();
+        boolean[][] visited = new boolean[table.length][table[0].length];
+        for(int i = 0 ; i < table.length ; i++) {
+            for(int j = 0 ; j < table[i].length ; j++) {
+                if(visited[i][j] || table[i][j] != target) continue;
+                Queue<int[]> queue = new ArrayDeque<>(); List<int[]> block = new ArrayList<>();
+                queue.offer(new int[]{i, j}); block.add(new int[]{i, j}); visited[i][j] = true;
+                while(!queue.isEmpty()) {
+                    int[] cur = queue.poll();
+                    for(int[] d : directions) {
+                        int dy = cur[0] + d[0], dx = cur[1] + d[1];
+                        if(dy < 0 || dx < 0 || dy >= table.length || dx >= table.length || visited[dy][dx]) continue;
+                        visited[dy][dx] = true;
+                        if(table[dy][dx] != target) continue;
+                        queue.offer(new int[]{dy, dx}); block.add(new int[]{dy, dx});
+                    }
+                }
+                answer.add(make(block));
+            }
+        }
+        return answer;
+    }
+    public int[][] make(List<int[]> block) {
+        int maxY = -1, maxX = -1, minY = 50, minX = 50, max = 0;
+        for(int[] b : block) {
+            maxY = Math.max(maxY, b[0]); maxX = Math.max(maxX, b[1]); 
+            minY = Math.min(minY, b[0]); minX = Math.min(minX, b[1]);
+        }
+        maxY -=  minY > 0 ? minY : 0; maxX -= minX > 0 ? minX : 0; max = Math.max(maxY, maxX);
+        int[][] answer = new int[max + 1][max + 1];
+        for(int[] b : block) answer[Math.min(b[0] - minY, b[0])][Math.min(b[1] - minX, b[1])] = 1;
+        return answer; 
+    }
+    public int[][] rotate(int[][] origin) {
+        int[][] answer = new int[origin.length][origin.length];
+        for(int i = 0 ; i < origin.length ; i++){
+            for(int j = 0 ; j < origin.length ; j++){
+                answer[i][j] = origin[origin.length - j - 1][i];
+            }
+        }
+        while(true){
+            int zeroLeftCnt = 0;
+            for(int i = 0 ; i < origin.length ; i++) zeroLeftCnt += answer[i][0] == 0 ? 1 : 0;
+            if(zeroLeftCnt != origin.length) break;
+            for(int i = 0 ; i < origin.length ; i++){
+                for(int j = 1 ; j < origin[0].length ; j++)
+                    answer[i][j - 1] = answer[i][j];
+                answer[i][origin.length - 1] = 0;
+            }
+        }
+        return answer;
+    }
+}
