afc4c4d9d1317720b7c9b69afaae510ff1e21af2
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2026-04-16 00:14:54 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 177.48 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
@@ -7,15 +7,14 @@ class Solution {
         int answer = 0; List<int[][]> block = collect(table, 1), target = collect(game_board, 0);
         boolean[] visitedB = new boolean[block.size()], visitedT = new boolean[target.size()];
         for(int i = 0 ; i < block.size() ; i++) {
-            int[][] b = block.get(i);
             for(int j = 0 ; j < target.size() ; j++) {
-                int[][] t = target.get(j);
-                for(int k = 0 ; k < 4 ; k++) {
-                    if(!visitedB[i] && !visitedT[j] && matches(t, b)) {
+                int[][] t = target.get(j), b = block.get(i);
+                if(visitedB[i] || visitedT[j]) continue;
+                for(int k = 0 ; k < 4 ; k++, t = rotate(t)) {
+                    if(matches(t, b)) {
                         answer += (int) Arrays.stream(t).flatMapToInt(Arrays::stream).sum(); 
                         visitedB[i] = true; visitedT[j] = true; break;
                     }
-                    t = rotate(t);
                 }
             }
         }
@@ -35,17 +34,15 @@ class Solution {
         boolean[][] visited = new boolean[table.length][table[0].length];
         for(int i = 0 ; i < table.length ; i++) {
             for(int j = 0 ; j < table[i].length ; j++) {
-                if(visited[i][j] || table[i][j] != target) continue;
-                Queue<int[]> queue = new ArrayDeque<>(); List<int[]> block = new ArrayList<>();
-                queue.offer(new int[]{i, j}); block.add(new int[]{i, j}); visited[i][j] = true;
-                while(!queue.isEmpty()) {
-                    int[] cur = queue.poll();
-                    for(int[] d : directions) {
-                        int dy = cur[0] + d[0], dx = cur[1] + d[1];
-                        if(dy < 0 || dx < 0 || dy >= table.length || dx >= table.length || visited[dy][dx]) continue;
-                        visited[dy][dx] = true;
-                        if(table[dy][dx] != target) continue;
-                        queue.offer(new int[]{dy, dx}); block.add(new int[]{dy, dx});
+                if (visited[i][j] || table[i][j] != target) continue;
+                List<int[]> block = new ArrayList<>(); Queue<int[]> queue = new ArrayDeque<>();
+                queue.offer(new int[]{i, j}); visited[i][j] = true;
+                while (!queue.isEmpty()) {
+                    int[] cur = queue.poll(); block.add(cur);
+                    for (int[] d : directions) {
+                        int ny = cur[0] + d[0], nx = cur[1] + d[1];
+                        if (ny < 0 || nx < 0 || ny >= table.length || nx >= table.length || visited[ny][nx] || table[ny][nx] != target) continue;
+                        visited[ny][nx] = true; queue.offer(new int[]{ny, nx});
                     }
                 }
                 answer.add(make(block));
@@ -54,23 +51,18 @@ class Solution {
         return answer;
     }
     public int[][] make(List<int[]> block) {
-        int maxY = -1, maxX = -1, minY = 50, minX = 50, max = 0;
-        for(int[] b : block) {
-            maxY = Math.max(maxY, b[0]); maxX = Math.max(maxX, b[1]); 
-            minY = Math.min(minY, b[0]); minX = Math.min(minX, b[1]);
-        }
-        maxY -=  minY; maxX -= minX; max = Math.max(maxY, maxX);
+        int maxY = block.stream().map(a -> a[0]).mapToInt(i -> i).max().getAsInt(), 
+            minY = block.stream().map(a -> a[0]).mapToInt(i -> i).min().getAsInt(),
+            maxX = block.stream().map(a -> a[1]).mapToInt(i -> i).max().getAsInt(), 
+            minX = block.stream().map(a -> a[1]).mapToInt(i -> i).min().getAsInt(),
+            max = Math.max(maxY -=  minY, maxX -= minX);
         int[][] answer = new int[max + 1][max + 1];
-        for(int[] b : block) answer[Math.min(b[0] - minY, b[0])][Math.min(b[1] - minX, b[1])] = 1;
+        for(int[] b : block) answer[b[0] - minY][b[1] - minX] = 1;
         return answer; 
     }
     public int[][] rotate(int[][] origin) {
         int[][] answer = new int[origin.length][origin.length];
-        for(int i = 0 ; i < origin.length ; i++){
-            for(int j = 0 ; j < origin.length ; j++){
-                answer[i][j] = origin[origin.length - j - 1][i];
-            }
-        }
+        IntStream.range(0, origin.length).forEach(i -> IntStream.range(0, origin.length).forEach(j -> answer[i][j] = origin[origin.length - j - 1][i]));
         while(IntStream.range(0, origin.length).filter(i -> answer[i][0] == 0).count() == origin.length){
             for(int i = 0 ; i < origin.length ; i++){
                 for(int j = 1 ; j < origin[0].length ; j++) {
